90892bd8c5ebcc1e00f5a9d33051a151e1ed3f93
Théophile MADET <theo.madet@gmail.com>
2021-03-30 22:34:45 +0200

    [accounts] Added template shift attendance to user details page.

--- tapir/shifts/templatetags/shifts.py
+++ tapir/shifts/templatetags/shifts.py
@@ -3,12 +3,13 @@ from builtins import enumerate
 from django import template
 
 from tapir.accounts.models import TapirUser
+from tapir.shifts.models import Shift, ShiftTemplate, ShiftAttendanceTemplate
 
 register = template.Library()
 
 
 @register.inclusion_tag("shifts/shift_block_tag.html", takes_context=True)
-def shift_block(context, shift):
+def shift_block(context, shift: Shift):
     context["shift"] = shift
 
     shift_attendance_states = ["empty" for _ in range(shift.num_slots)]
@@ -16,10 +17,22 @@ def shift_block(context, shift):
 
     for index, attendance in enumerate(shift_attendances):
         shift_attendance_states[index] = "filled"
-        if (
-            isinstance(context["user"], TapirUser)
-            and attendance.user.id == context["user"].id
-        ):
+        if isinstance(context["user"], TapirUser) and attendance.user.id == context["user"].id:
             shift_attendance_states[0] = "user"
     context["shift_attendance_states"] = shift_attendance_states
+    return context
+
+
+@register.inclusion_tag("shifts/shift_template_block_tag.html", takes_context=True)
+def shift_template_block(context, shift_template: ShiftTemplate):
+    context["shift_template"] = shift_template
+
+    attendance_states = ["empty" for _ in range(shift_template.num_slots)]
+    attendances = ShiftAttendanceTemplate.objects.filter(shift_template=shift_template)
+    for index, attendance in enumerate(attendances):
+        attendance_states[index] = "filled"
+        if isinstance(context["user"], TapirUser) and attendance.user.id == context["user"].id:
+            attendance_states[0] = "user"
+
+    context["attendance_states"] = attendance_states
     return context
