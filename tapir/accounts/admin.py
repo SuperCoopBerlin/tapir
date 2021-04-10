c7eea998e9970cca76608801517525e31ff1eefa
Théophile MADET <theo.madet@gmail.com>
2021-04-10 18:14:39 +0200

    [admin] Added columns and search to user admin page

--- tapir/accounts/admin.py
+++ tapir/accounts/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from tapir.accounts.models import TapirUser
 
-admin.site.register(TapirUser)
+
+@admin.register(TapirUser)
+class TapirUserAdmin(admin.ModelAdmin):
+    list_display = ["username", "is_staff", "is_active", "email"]
+    list_filter = ("is_staff", "is_active")
+    search_fields = ["username", "first_name", "last_name", "email"]
