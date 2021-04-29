071428ef7ac791648253107a0679c9254043bd52
Leon Handreke <leonh@ndreke.de>
2021-04-29 10:57:25 +0200

    Really fix template path

--- tapir/settings.py
+++ tapir/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = "tapir.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "tapir" / "static"],
+        "DIRS": [BASE_DIR / "tapir" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
