f1d98f5688d98cb752dac26cdd731a387c3fa0e7
Théophile MADET <theo.madet@gmail.com>
2021-05-02 19:56:00 +0200

    [coop] share owner detail page now has the list of shares.

--- tapir/coop/templatetags/coop.py
+++ tapir/coop/templatetags/coop.py
@@ -1,5 +1,7 @@
 from django import template
 
+from tapir.coop.models import ShareOwner
+
 register = template.Library()
 
 
@@ -13,4 +15,13 @@ def user_coop_share_ownership_list(context, user):
         context["coop_share_ownerships"] = user.coop_share_owner.share_ownerships.all()
     else:
         context["coop_share_ownerships"] = []
+    return context
+
+
+@register.inclusion_tag(
+    "coop/user_coop_share_ownership_list_tag.html", takes_context=True
+)
+def share_owner_ownership_list(context, share_owner: ShareOwner):
+    context["coop_share_owner"] = share_owner
+    context["coop_share_ownerships"] = share_owner.share_ownerships.all()
     return context
