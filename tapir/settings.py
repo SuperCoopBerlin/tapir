732ed1af66e83a29f2744226e70db581c7e5c092
Leon Handreke <leonh@ndreke.de>
2021-05-28 19:26:49 +0200

    [coop] Use postgres in development

--- tapir/settings.py
+++ tapir/settings.py
@@ -85,8 +85,11 @@ WSGI_APPLICATION = "tapir.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": "db",
+        "USER": "tapir",
+        "PASSWORD": "tapir",
+        "NAME": "tapir",
     },
     "ldap": {
         "ENGINE": "ldapdb.backends.ldap",
