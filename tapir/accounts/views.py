26f61e066e2a8808f762a3ea52444f744bab3077
Leon Handreke <leonh@ndreke.de>
2021-06-07 17:09:47 +0200

    [accounts] Fix user view

--- tapir/accounts/views.py
+++ tapir/accounts/views.py
@@ -21,20 +21,6 @@ class UserDetailView(PermissionRequiredMixin, generic.DetailView):
     template_name = "accounts/user_detail.html"
     permission_required = "accounts.manage"
 
-    def get_context_data(self, *args, **kwargs):
-        context_data = super().get_context_data(*args, **kwargs)
-        max_date = datetime.datetime.now() + datetime.timedelta(days=90)
-        user: TapirUser = context_data["user"]
-        context_data["shift_attendances"] = ShiftAttendance.objects.filter(
-            user=user,
-            state=ShiftAttendance.State.PENDING,
-            shift__start_time__lt=max_date,
-        ).order_by("shift__start_time")
-        context_data[
-            "shift_template_attendances"
-        ] = ShiftAttendanceTemplate.objects.filter(user=user)
-        return context_data
-
 
 class UserMeView(LoginRequiredMixin, generic.RedirectView):
     def get_redirect_url(self, *args, **kwargs):
