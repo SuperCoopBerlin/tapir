212f0f1ee2bcfa44dcf9789e8fbc3b1299f4dc6f
Leon Handreke <leonh@ndreke.de>
2021-06-07 11:08:37 +0200

    [coop] Properly deal with users that are not a member

--- tapir/coop/templatetags/coop.py
+++ tapir/coop/templatetags/coop.py
@@ -8,16 +8,6 @@ register = template.Library()
 @register.inclusion_tag(
     "coop/user_coop_share_ownership_list_tag.html", takes_context=True
 )
-def user_coop_share_ownership_list(context, user):
-    context["user"] = user
-    if hasattr(user, "share_owner"):
-        context["owner"] = user.share_owner
-    return context
-
-
-@register.inclusion_tag(
-    "coop/user_coop_share_ownership_list_tag.html", takes_context=True
-)
 def share_owner_ownership_list(context, share_owner: ShareOwner):
     context["owner"] = share_owner
     return context
