79d019ab90c6f22a355411875afb1c85c40e3818
Cornelius <ces92@users.noreply.github.com>
2021-07-16 21:03:05 +0200

    Add basic tests for admin

---
+++ tapir/test_project.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.test import Client
+
+from tapir.utils.tests_utils import LdapEnabledTestCase
+
+
+class TestProject(LdapEnabledTestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_superuser(
+            username="admin", email="admin@admin.de", password="admin"
+        )
+        self.client = Client()
+        self.client.force_login(self.user)
+
+    def test_model_admins(self):
+        model_admin_urls = [
+            f"/admin/{model._meta.app_label}/{model._meta.model_name}/"
+            for model, model_admin in admin.site._registry.items()
+        ]
+        for url in model_admin_urls:
+            response = self.client.get(url, follow=True)
+            self.assertEqual(response.status_code, 200)
