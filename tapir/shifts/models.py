80eafbad481bb9adc905ef32d160b21394272820
Théophile MADET <theo.madet@posteo.net>
2021-08-29 21:39:06 +0200

    Only affect future attendances.
    Co-authored-by: Leon Handreke <leonh@ndreke.de>

--- tapir/shifts/models.py
+++ tapir/shifts/models.py
@@ -256,7 +256,7 @@ class ShiftAttendanceTemplate(models.Model):
 def on_shift_attendance_template_delete(
     sender, instance: ShiftAttendanceTemplate, using, **kwargs
 ):
-    slots = ShiftSlot.objects.filter(slot_template=instance.slot_template)
+    slots = instance.slot_template.generated_slots()
     for slot in slots:
         attendances = ShiftAttendance.objects.filter(slot=slot)
         for attendance in slot.attendances.filter(start_date__gte=timezone.now(), user=instance.user):
