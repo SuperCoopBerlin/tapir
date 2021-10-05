ef29b52f32b8ee580c466d6b37873c215b3717c2
Théophile MADET <theo.madet@posteo.net>
2021-10-05 21:50:05 +0200

    Changed timezone from BerlinTime to Etc/GMT-2 so that daylight savings don't mess up the shift times.

--- tapir/settings.py
+++ tapir/settings.py
@@ -163,7 +163,7 @@ PASSWORD_RESET_TIMEOUT = (
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
-TIME_ZONE = "Europe/Berlin"
+TIME_ZONE = "Etc/GMT-2"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
