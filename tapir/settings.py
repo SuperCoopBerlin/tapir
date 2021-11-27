e45ad51c66832c1f41234cd402c95ea1040dcdb6
Théophile MADET <theo.madet@posteo.net>
2021-11-27 09:26:24 +0100

    Set the default language when not logged in to german.

--- tapir/settings.py
+++ tapir/settings.py
@@ -162,7 +162,7 @@ PASSWORD_RESET_TIMEOUT = (
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "de"
 TIME_ZONE = "Etc/GMT-2"
 USE_I18N = True
 USE_L10N = True
