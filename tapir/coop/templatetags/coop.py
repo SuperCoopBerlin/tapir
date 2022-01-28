d824b4eaed21d692261f6f85a5facd657444ac60
Théophile Madet <theo.madet@posteo.net>
2022-01-28 15:14:01 +0100

    WIP share_counter

--- tapir/coop/templatetags/coop.py
+++ tapir/coop/templatetags/coop.py
@@ -1,6 +1,8 @@
+import datetime
+
 from django import template
 
-from tapir.coop.models import ShareOwner
+from tapir.coop.models import ShareOwner, ShareOwnership
 
 register = template.Library()
 
@@ -10,4 +12,18 @@ register = template.Library()
 )
 def share_owner_ownership_list(context, share_owner: ShareOwner):
     context["owner"] = share_owner
+    return context
+
+
+@register.inclusion_tag("coop/shop_extension_share_count.html", takes_context=True)
+def shop_extension_share_count(context):
+    nb_shares_at_start = ShareOwnership.objects.active_temporal(
+        datetime.date(day=1, month=1, year=2022)
+    ).count()
+    nb_shares_now = ShareOwnership.objects.active_temporal().count()
+    context["shop_extension_current"] = nb_shares_now - nb_shares_at_start
+    context["shop_extension_max"] = 200
+    context["shop_extension_progress"] = (
+        context["shop_extension_current"] / context["shop_extension_max"]
+    ) * 100
     return context
