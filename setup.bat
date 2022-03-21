9cf8d4cf06bf981de283ea5f61d91a758781d0f7
Théophile Madet <theo.madet@posteo.net>
2022-03-21 14:24:40 +0100

    Deleted the browser shift tests.

--- setup.bat
+++
@@ -1,3 +0,0 @@
-docker-compose exec web poetry run python manage.py migrate
-docker-compose exec web poetry run python manage.py loaddata admin_account
-docker-compose exec web poetry run python manage.py populate --reset_all
