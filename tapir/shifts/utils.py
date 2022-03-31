fe3220ba79c8c69f11b8f0065791c97d5828c9f1
Théophile Madet <theo.madet@posteo.net>
2022-03-31 15:35:50 +0200

    Updated generate_shifts_up_to with an optional start_date.

--- tapir/shifts/utils.py
+++ tapir/shifts/utils.py
@@ -1,13 +1,14 @@
 from datetime import datetime, timedelta, date
 
 from tapir.shifts.templatetags.shifts import get_week_group
+from tapir.utils.shortcuts import get_monday
 
 
-def generate_shifts_up_to(target_day: datetime.date):
-    target_monday = target_day - timedelta(days=target_day.weekday())
-    current_monday = date.today() - timedelta(days=date.today().weekday())
+def generate_shifts_up_to(end_date: datetime.date, start_date=date.today()):
+    last_monday = get_monday(end_date)
+    current_monday = get_monday(start_date)
 
-    while current_monday < target_monday:
+    while current_monday < last_monday:
         current_monday += timedelta(days=7)
         group = get_week_group(current_monday)
         group.create_shifts(current_monday)
