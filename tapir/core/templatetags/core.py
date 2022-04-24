2c28af9eedfc5597b49ed49582cc3943e58f9ac3
Frederik <18083323+crosspolar@users.noreply.github.com>
2022-04-24 20:34:03 +0200

    rename to annual calendar

--- tapir/core/templatetags/core.py
+++ tapir/core/templatetags/core.py
@@ -78,7 +78,7 @@ def get_sidebar_link_groups(request):
     )
     shifts_group.add_link(
         display_name=_(
-            "ABCD weeks calendar, current week: {current_week_group_name}"
+            "ABCD annual calendar, current week: {current_week_group_name}"
         ).format(current_week_group_name=current_week_group_name),
         material_icon="table_view",
         url=reverse_lazy("shifts:shift_template_group_calendar"),
