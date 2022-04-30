0671cbd91a045ed8c5cc74af9e4f34565de13449
Théophile Madet <theo.madet@posteo.net>
2022-04-30 13:42:15 +0200

    Reverted black formatting ignoring migrations files : it's required in deployment and automated testing.

--- tapir/shifts/migrations/0035_auto_20220430_0015.py
+++ tapir/shifts/migrations/0035_auto_20220430_0015.py
@@ -7,23 +7,70 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('shifts', '0034_auto_20220417_0947'),
+        ("shifts", "0034_auto_20220417_0947"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='shiftslot',
-            name='required_capabilities',
-            field=django.contrib.postgres.fields.ArrayField(base_field=models.CharField(choices=[('shift_coordinator', 'Teamleader'), ('cashier', 'Cashier'), ('member_office', 'Member Office'), ('bread_delivery', 'Bread Delivery'), ('red_card', 'Red Card'), ('first_aid', 'First Aid'), ('welcome_session', 'Welcome Session')], max_length=128), blank=True, default=list, size=None),
+            model_name="shiftslot",
+            name="required_capabilities",
+            field=django.contrib.postgres.fields.ArrayField(
+                base_field=models.CharField(
+                    choices=[
+                        ("shift_coordinator", "Teamleader"),
+                        ("cashier", "Cashier"),
+                        ("member_office", "Member Office"),
+                        ("bread_delivery", "Bread Delivery"),
+                        ("red_card", "Red Card"),
+                        ("first_aid", "First Aid"),
+                        ("welcome_session", "Welcome Session"),
+                    ],
+                    max_length=128,
+                ),
+                blank=True,
+                default=list,
+                size=None,
+            ),
         ),
         migrations.AlterField(
-            model_name='shiftslottemplate',
-            name='required_capabilities',
-            field=django.contrib.postgres.fields.ArrayField(base_field=models.CharField(choices=[('shift_coordinator', 'Teamleader'), ('cashier', 'Cashier'), ('member_office', 'Member Office'), ('bread_delivery', 'Bread Delivery'), ('red_card', 'Red Card'), ('first_aid', 'First Aid'), ('welcome_session', 'Welcome Session')], max_length=128), blank=True, default=list, size=None),
+            model_name="shiftslottemplate",
+            name="required_capabilities",
+            field=django.contrib.postgres.fields.ArrayField(
+                base_field=models.CharField(
+                    choices=[
+                        ("shift_coordinator", "Teamleader"),
+                        ("cashier", "Cashier"),
+                        ("member_office", "Member Office"),
+                        ("bread_delivery", "Bread Delivery"),
+                        ("red_card", "Red Card"),
+                        ("first_aid", "First Aid"),
+                        ("welcome_session", "Welcome Session"),
+                    ],
+                    max_length=128,
+                ),
+                blank=True,
+                default=list,
+                size=None,
+            ),
         ),
         migrations.AlterField(
-            model_name='shiftuserdata',
-            name='capabilities',
-            field=django.contrib.postgres.fields.ArrayField(base_field=models.CharField(choices=[('shift_coordinator', 'Teamleader'), ('cashier', 'Cashier'), ('member_office', 'Member Office'), ('bread_delivery', 'Bread Delivery'), ('red_card', 'Red Card'), ('first_aid', 'First Aid'), ('welcome_session', 'Welcome Session')], max_length=128), default=list, size=None),
+            model_name="shiftuserdata",
+            name="capabilities",
+            field=django.contrib.postgres.fields.ArrayField(
+                base_field=models.CharField(
+                    choices=[
+                        ("shift_coordinator", "Teamleader"),
+                        ("cashier", "Cashier"),
+                        ("member_office", "Member Office"),
+                        ("bread_delivery", "Bread Delivery"),
+                        ("red_card", "Red Card"),
+                        ("first_aid", "First Aid"),
+                        ("welcome_session", "Welcome Session"),
+                    ],
+                    max_length=128,
+                ),
+                default=list,
+                size=None,
+            ),
         ),
     ]
