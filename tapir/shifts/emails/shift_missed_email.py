4e99e407ea7d5b338d33429bb56f6b9db7f86fd4
Théophile Madet <theophile.madet@researchgate.net>
2022-08-29 16:02:42 +0200

    Converted the shift missed email to the new email class.

---
+++ tapir/shifts/emails/shift_missed_email.py
@@ -0,0 +1,60 @@
+from typing import List
+
+from django.utils.translation import gettext_lazy as _
+
+from tapir import settings
+from tapir.coop.models import ShareOwner
+from tapir.core.tapir_email_base import TapirEmailBase, all_emails
+from tapir.shifts.models import Shift
+
+
+class ShiftMissedEmail(TapirEmailBase):
+    shift = None
+
+    def __init__(self, shift):
+        self.shift = shift
+
+    @classmethod
+    def get_unique_id(cls) -> str:
+        return "tapir.shifts.shift_missed"
+
+    @classmethod
+    def get_name(cls) -> str:
+        return _("Shift missed")
+
+    @classmethod
+    def get_description(cls) -> str:
+        return _("Sent to a member when the member office marks the shift as missed")
+
+    def get_subject_templates(self) -> List:
+        return [
+            "shifts/email/shift_missed.subject.html",
+            "shifts/email/shift_missed.subject.default.html",
+        ]
+
+    def get_body_templates(self) -> List:
+        return [
+            "shifts/email/shift_missed.body.html",
+            "shifts/email/shift_missed.body.default.html",
+        ]
+
+    def get_extra_context(self) -> dict:
+        return {
+            "shift": self.shift,
+            "contact_email_address": settings.EMAIL_ADDRESS_MEMBER_OFFICE,
+            "coop_name": settings.COOP_NAME,
+        }
+
+    @classmethod
+    def get_dummy_version(cls) -> TapirEmailBase:
+        share_owner = ShareOwner.objects.filter(user__isnull=False).order_by("?")[0]
+        mail = cls(shift=Shift.objects.order_by("?")[0])
+        mail.get_full_context(
+            share_owner=share_owner,
+            member_infos=share_owner.get_info(),
+            tapir_user=share_owner.user,
+        )
+        return mail
+
+
+all_emails[ShiftMissedEmail.get_unique_id()] = ShiftMissedEmail
