c487e77d7b7cbcd6cf9061f0aeffdf17a09dc8a1
Théophile Madet <theo.madet@posteo.net>
2022-09-24 09:13:14 +0200

    Fixed migrations conflicting after merge in PR #292

--- tapir/coop/migrations/0031_newmembersandsharesemailrecaplogs.py
+++ tapir/coop/migrations/0031_newmembersandsharesemailrecaplogs.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("coop", "0029_auto_20220617_1056"),
+        ("coop", "0030_createpaymentlogentry"),
     ]
 
     operations = [
