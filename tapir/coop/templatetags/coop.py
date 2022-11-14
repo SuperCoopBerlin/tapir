db80cd8586a1dd0a27c7bea84294a13e717b2d60
Théophile MADET <theo.madet@posteo.net>
2022-11-14 18:51:50 +0100

    First draft for a member count progress bar.

--- tapir/coop/templatetags/coop.py
+++ tapir/coop/templatetags/coop.py
@@ -1,7 +1,9 @@
 from django import template
 
+from tapir.accounts.models import TapirUser
 from tapir.coop.models import (
     ShareOwner,
+    MemberStatus,
 )
 
 register = template.Library()
@@ -12,4 +14,20 @@ register = template.Library()
 )
 def share_owner_ownership_list(context, share_owner: ShareOwner):
     context["share_owner"] = share_owner
+    return context
+
+
+@register.inclusion_tag("coop/active_members_progress_bar.html", takes_context=True)
+def active_members_progress_bar(context):
+    active_members = ShareOwner.objects.with_status(MemberStatus.ACTIVE)
+
+    context["active_members_with_account_count"] = TapirUser.objects.filter(
+        share_owner__in=active_members
+    ).count()
+    context["target_member_count"] = 1000
+    context["progress"] = round(
+        100
+        * context["active_members_with_account_count"]
+        / context["target_member_count"]
+    )
     return context
