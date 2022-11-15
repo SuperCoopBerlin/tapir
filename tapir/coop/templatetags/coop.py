cdeffe72157396ba3078e2afb67e78b106b2b6c3
Théophile MADET <theo.madet@posteo.net>
2022-11-15 15:41:27 +0100

    Member count progress bar update.

--- tapir/coop/templatetags/coop.py
+++ tapir/coop/templatetags/coop.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django import template
 
 from tapir.accounts.models import TapirUser
@@ -19,15 +21,28 @@ def share_owner_ownership_list(context, share_owner: ShareOwner):
 
 @register.inclusion_tag("coop/active_members_progress_bar.html", takes_context=True)
 def active_members_progress_bar(context):
-    active_members = ShareOwner.objects.with_status(MemberStatus.ACTIVE)
+    total_active_members = ShareOwner.objects.with_status(MemberStatus.ACTIVE)
+    active_members_on_start_date = ShareOwner.objects.with_status(
+        MemberStatus.ACTIVE, datetime.date(year=2012, month=11, day=15)
+    )
 
-    context["active_members_with_account_count"] = TapirUser.objects.filter(
-        share_owner__in=active_members
+    member_count_on_start_date = TapirUser.objects.filter(
+        share_owner__in=active_members_on_start_date
     ).count()
-    context["target_member_count"] = 1000
-    context["progress"] = round(
-        100
-        * context["active_members_with_account_count"]
-        / context["target_member_count"]
+    context["member_count_on_start_date"] = member_count_on_start_date
+
+    new_member_count_since_start_date = (
+        TapirUser.objects.filter(share_owner__in=total_active_members).count()
+        - member_count_on_start_date
+    )
+    context["new_member_count_since_start_date"] = new_member_count_since_start_date
+
+    target_member_count = 1000
+    context["target_member_count"] = target_member_count
+    context["progress_on_start_date"] = round(
+        100 * member_count_on_start_date / target_member_count
+    )
+    context["progress_since_start_date"] = round(
+        100 * new_member_count_since_start_date / target_member_count
     )
     return context
