2ce819d033802ad2f3821e1d04ae851d824c54ab
Théophile MADET <theo.madet@posteo.net>
2023-02-22 12:07:24 +0100

    Updated table template to use bootstrap5

--- tapir/core/config.py
+++ tapir/core/config.py
@@ -1,6 +1,6 @@
 from tapir.core.models import SidebarLinkGroups
 
 TAPIR_TABLE_CLASSES = "table table-striped table-hover"
-TAPIR_TABLE_TEMPLATE = "django_tables2/bootstrap4.html"
+TAPIR_TABLE_TEMPLATE = "django_tables2/bootstrap5.html"
 
 sidebar_link_groups = SidebarLinkGroups()
