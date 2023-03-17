ac42d44795e26552dad0bb1d12a87d0ab5114e70
Théophile MADET <theo.madet@posteo.net>
2023-03-17 17:02:16 +0100

    Updated member-shift-capability filters to make it clear which filters looks at ABCD shift and which filter looks at normal shifts.

--- tapir/coop/config.py
+++ tapir/coop/config.py
@@ -7,3 +7,6 @@ COOP_DEFAULT_SHARES = 5
 COOP_MAX_SHARES = 5000
 
 on_welcome_session_attendance_update = []
+# This is not very clean, we need a better solution to inject member filters from the shift app into the coop app
+# without adding a dependency from the coop app to the shift app
+get_ids_of_users_registered_to_a_shift_with_capability = []
