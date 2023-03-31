7630d1dbc1ded7ecbd006645102d693742d8dabe
Théophile Madet <theophile.madet@researchgate.net>
2023-03-31 16:56:52 +0200

    Small naming improvement

--- tapir/shifts/tests/test_ShiftUserData.py
+++ tapir/shifts/tests/test_ShiftUserData.py
@@ -7,17 +7,17 @@ from tapir.utils.tests_utils import TapirFactoryTestBase
 
 class TestShiftUserData(TapirFactoryTestBase):
     def test_canShop_userWithPositiveBalance_canShop(self):
-        user = TapirUserFactory.create()
-        ShiftAccountEntry.objects.create(user=user, date=timezone.now(), value=2)
-        self.assertTrue(user.shift_user_data.can_shop())
+        tapir_user = TapirUserFactory.create()
+        ShiftAccountEntry.objects.create(user=tapir_user, date=timezone.now(), value=2)
+        self.assertTrue(tapir_user.shift_user_data.can_shop())
 
     def test_canShop_userWithNegativeBalance_cannotShop(self):
-        user = TapirUserFactory.create()
-        ShiftAccountEntry.objects.create(user=user, date=timezone.now(), value=-2)
-        self.assertFalse(user.shift_user_data.can_shop())
+        tapir_user = TapirUserFactory.create()
+        ShiftAccountEntry.objects.create(user=tapir_user, date=timezone.now(), value=-2)
+        self.assertFalse(tapir_user.shift_user_data.can_shop())
 
     def test_canShop_userWithFrozenAttendanceMode_cannotShop(self):
-        user = TapirUserFactory.create()
-        user.shift_user_data.attendance_mode = ShiftAttendanceMode.FROZEN
-        ShiftAccountEntry.objects.create(user=user, date=timezone.now(), value=2)
-        self.assertFalse(user.shift_user_data.can_shop())
+        tapir_user = TapirUserFactory.create()
+        tapir_user.shift_user_data.attendance_mode = ShiftAttendanceMode.FROZEN
+        ShiftAccountEntry.objects.create(user=tapir_user, date=timezone.now(), value=2)
+        self.assertFalse(tapir_user.shift_user_data.can_shop())
