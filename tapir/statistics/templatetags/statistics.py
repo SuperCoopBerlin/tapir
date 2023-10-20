319bf024f3f1de447c9c5603c750a6515df22cd9
Théophile Madet <theo.madet@posteo.net>
2023-10-20 19:12:01 +0200

    Added a progress bar for monthly basket to stats.

--- tapir/statistics/templatetags/statistics.py
+++ tapir/statistics/templatetags/statistics.py
@@ -1,8 +1,7 @@
 from django import template
-from django.db.models import Sum
-from django.utils import timezone
 
 from tapir.statistics.models import PurchaseBasket
+from tapir.statistics.views import get_average_monthly_basket
 
 register = template.Library()
 
@@ -13,20 +12,8 @@ register = template.Library()
 def purchase_statistics_card(context, tapir_user):
     user_purchases = PurchaseBasket.objects.filter(tapir_user=tapir_user)
     context["last_purchases"] = user_purchases.order_by("-purchase_date")[:10]
-
-    first_purchase = user_purchases.order_by("purchase_date").first()
-    if not first_purchase:
-        context["average_basket_per_month"] = 0
-        return context
-
-    first_purchase: PurchaseBasket
-    days_since_first_purchase = (
-        timezone.now().date() - first_purchase.purchase_date.date()
-    ).days
-    average_days_per_month = 365.25 / 12
-    months_since_first_purchase = days_since_first_purchase / average_days_per_month
     context["average_basket_per_month"] = "{:.2f}".format(
-        user_purchases.aggregate(total_paid=Sum("gross_amount"))["total_paid"]
-        / months_since_first_purchase
+        get_average_monthly_basket(user_purchases)
     )
+
     return context
