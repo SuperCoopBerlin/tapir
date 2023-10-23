590422fd84f33b417a4d7e525d0d842b7238ac4e
Théophile Madet <theo.madet@posteo.net>
2023-10-23 18:40:11 +0200

    Updated the shift exemption page to use filtering and sorting.

--- tapir/shifts/views/exemptions.py
+++ tapir/shifts/views/exemptions.py
@@ -1,14 +1,23 @@
+import django_filters
+import django_tables2
 from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from django.db import transaction
-from django.urls import reverse
+from django.db.models import QuerySet
+from django.urls import reverse, reverse_lazy
+from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import (
     CreateView,
     UpdateView,
     ListView,
 )
+from django_filters.views import FilterView
+from django_tables2 import SingleTableView
+from django_tables2.export import ExportMixin
 
 from tapir.accounts.models import TapirUser
+from tapir.core.config import TAPIR_TABLE_TEMPLATE, TAPIR_TABLE_CLASSES
+from tapir.core.templatetags.core import tapir_button_link_to_action
 from tapir.core.views import TapirFormMixin
 from tapir.log.util import freeze_for_log
 from tapir.log.views import UpdateViewLogMixin
@@ -139,7 +148,7 @@ class EditShiftExemptionView(
         return context
 
 
-class ShiftExemptionListView(LoginRequiredMixin, ListView):
+class ShiftExemptionListView2(LoginRequiredMixin, ListView):
     model = ShiftExemption
 
     def get_queryset(self):
@@ -161,3 +170,122 @@ class ShiftExemptionListView(LoginRequiredMixin, ListView):
                 pk=shift_user_data_id
             )
         return context_data
+
+
+class ShiftExemptionTable(django_tables2.Table):
+    class Meta:
+        model = ShiftExemption
+        template_name = TAPIR_TABLE_TEMPLATE
+        fields = [
+            "id",
+            "shift_user_data",
+            "description",
+            "start_date",
+            "end_date",
+        ]
+        sequence = (
+            "id",
+            "shift_user_data",
+            "description",
+            "start_date",
+            "end_date",
+        )
+        order_by = "id"
+        attrs = {"class": TAPIR_TABLE_CLASSES}
+
+    shift_user_data = django_tables2.Column(verbose_name="Member")
+
+    edit_button = django_tables2.Column(
+        empty_values=(),
+        verbose_name="Actions",
+        orderable=False,
+        exclude_from_export=True,
+    )
+
+    def before_render(self, request):
+        self.request = request
+
+    def render_shift_user_data(self, value, record: ShiftExemption):
+        return UserUtils.build_html_link_for_viewer(
+            record.shift_user_data.user, self.request.user
+        )
+
+    def value_shift_user_data(self, value, record: ShiftExemption):
+        return record.shift_user_data.user.get_member_number()
+
+    def render_start_date(self, value, record: ShiftExemption):
+        return record.start_date.strftime("%d.%m.%Y")
+
+    def render_end_date(self, value, record: ShiftExemption):
+        return record.end_date.strftime("%d.%m.%Y") if record.end_date else _("None")
+
+    def render_edit_button(self, value, record: ShiftExemption):
+        return format_html(
+            "<a href='{}' class='{}'>{}</a>",
+            reverse_lazy("shifts:edit_shift_exemption", args=[record.pk]),
+            tapir_button_link_to_action(),
+            format_html("<span class='material-icons'>edit</span>"),
+        )
+
+
+class ShiftExemptionFilter(django_filters.FilterSet):
+    class Meta:
+        model = ShiftExemption
+        fields = []
+
+    is_active = django_filters.BooleanFilter(
+        method="is_active_filter", label="Is active"
+    )
+
+    @staticmethod
+    def is_active_filter(queryset: QuerySet, name, is_active: bool):
+        if is_active:
+            return queryset.active_temporal()
+        else:
+            return queryset.exclude(id__in=queryset.active_temporal())
+
+
+class ShiftExemptionListView(
+    LoginRequiredMixin,
+    FilterView,
+    ExportMixin,
+    SingleTableView,
+):
+    table_class = ShiftExemptionTable
+    model = ShiftExemption
+    template_name = "shifts/shiftexemption_list.html"
+    filterset_class = ShiftExemptionFilter
+    export_formats = ["csv", "json"]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if not self.request.user.has_perm(PERMISSION_SHIFTS_EXEMPTIONS):
+            queryset = queryset.filter(
+                shift_user_data__id=self.request.user.shift_user_data.id
+            )
+        shift_user_data_id = self.request.GET.get("shift_user_data_id", None)
+        if shift_user_data_id is not None:
+            queryset = queryset.filter(shift_user_data__id=shift_user_data_id)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data["filtered_exemption_count"] = self.object_list.count()
+        context_data["total_exemption_count"] = ShiftExemption.objects.count()
+        shift_user_data_id = self.request.GET.get("shift_user_data_id", None)
+        if shift_user_data_id is not None:
+            context_data["shift_user_data"] = ShiftUserData.objects.get(
+                pk=shift_user_data_id
+            )
+        return context_data
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if not self.request.user.has_perm(PERMISSION_SHIFTS_EXEMPTIONS):
+            queryset = queryset.filter(
+                shift_user_data__id=self.request.user.shift_user_data.id
+            )
+        shift_user_data_id = self.request.GET.get("shift_user_data_id", None)
+        if shift_user_data_id is not None:
+            queryset = queryset.filter(shift_user_data__id=shift_user_data_id)
+        return queryset
