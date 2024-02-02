a6923247659356604560b1ccad14937b45353928
Théophile Madet <theo.madet@posteo.net>
2024-02-02 16:48:12 +0100

    Use pg_dump to export instead of csv files.

--- tapir/core/management/commands/metabase_export.py
+++ tapir/core/management/commands/metabase_export.py
@@ -1,15 +1,8 @@
-import csv
-import shutil
-from pathlib import Path
+import os
 
-from django.apps import apps
-from django.contrib.auth.models import User, Group, Permission
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.sessions.models import Session
+import environ
 from django.core.management.base import BaseCommand
 
-from tapir.accounts.models import LdapPerson, LdapGroup
-from tapir.log.models import LogEntry
 from tapir.utils.shortcuts import (
     send_file_to_storage_server,
 )
@@ -21,35 +14,51 @@ class Command(BaseCommand):
         "Those files will then be imported by our metabase instance."
     )
 
-    do_not_export = [
-        ContentType,
-        Group,
-        LdapGroup,
-        LdapPerson,
-        LogEntry,
-        Permission,
-        Session,
-        User,
+    exported_tables = [
+        "accounts_tapiruser",
+        "accounts_updatetapiruserlogentry",
+        "coop_createpaymentlogentry",
+        "coop_createshareownershipslogentry",
+        "coop_deleteshareownershiplogentry",
+        "coop_draftuser",
+        "coop_extrasharesforaccountingrecap",
+        "coop_incomingpayment",
+        "coop_membershippause",
+        "coop_membershippausecreatedlogentry",
+        "coop_membershippauseupdatedlogentry",
+        "coop_newmembershipsforaccountingrecap",
+        "coop_shareowner",
+        "coop_shareownership",
+        "coop_updateshareownerlogentry",
+        "coop_updateshareownershiplogentry",
+        "shifts_createexemptionlogentry",
+        "shifts_createshiftattendancelogentry",
+        "shifts_createshiftattendancetemplatelogentry",
+        "shifts_deleteshiftattendancetemplatelogentry",
+        "shifts_shift",
+        "shifts_shiftaccountentry",
+        "shifts_shiftattendance",
+        "shifts_shiftattendancetakenoverlogentry",
+        "shifts_shiftattendancetemplate",
+        "shifts_shiftcycleentry",
+        "shifts_shiftexemption",
+        "shifts_shiftslot",
+        "shifts_shiftslottemplate",
+        "shifts_shifttemplate",
+        "shifts_shifttemplategroup",
+        "shifts_shiftuserdata",
+        "shifts_solidarityshift",
+        "shifts_updateexemptionlogentry",
+        "shifts_updateshiftattendancestatelogentry",
+        "shifts_updateshiftuserdatalogentry",
     ]
 
     def handle(self, *args, **options):
-        Path("./exports").mkdir(parents=True, exist_ok=True)
-        for model in apps.get_models(include_auto_created=True, include_swapped=True):
-            if model in self.do_not_export:
-                continue
-            self.export_model(model)
-
-        shutil.make_archive("tapir_exports", "zip", "exports")
-        send_file_to_storage_server("tapir_exports.zip", "u326634-sub7")
-
-    @staticmethod
-    def export_model(model):
-        with open(f"./exports/{model.__name__}.csv", "w", newline="") as csvfile:
-            writer = csv.writer(csvfile, delimiter=";", quoting=csv.QUOTE_MINIMAL)
-
-            fields = model._meta.get_fields()
-            fields = [field for field in fields if hasattr(field, "value_from_object")]
-            writer.writerow([field.name for field in fields])
-
-            for instance in model.objects.order_by("pk"):
-                writer.writerow([field.value_from_object(instance) for field in fields])
+        FILENAME = "tapir_dump.sql"
+        env = environ.Env()
+        password = env.db(default="postgresql://tapir:tapir@db:5432/tapir")["PASSWORD"]
+        os.system(
+            f"PGPASSWORD='{password}' pg_dump --file='{FILENAME}' --no-owner --host=db --username=tapir tapir "
+            + " ".join([f"-t {table_name}" for table_name in self.exported_tables])
+        )
+        send_file_to_storage_server(FILENAME, "u326634-sub7")
