94f99f6d194779a8d4988253a486e5bbdfe16a85
Théophile Madet <theo.madet@posteo.net>
2024-07-01 16:17:52 +0200

    Fixed missing import in apply_shift_cycle_start.py

--- tapir/shifts/management/commands/apply_shift_cycle_start.py
+++ tapir/shifts/management/commands/apply_shift_cycle_start.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.core.management.base import BaseCommand
 from django.db.models import Max
+from django.utils import timezone
 
 from tapir.shifts.models import ShiftCycleEntry, ShiftTemplateGroup, Shift
 from tapir.shifts.services.shift_cycle_service import ShiftCycleService
