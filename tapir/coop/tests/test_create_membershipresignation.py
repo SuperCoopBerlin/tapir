40a3929f1398c9df6bff0eab2ef3f6d2d724c763
Kersten Weise <kerstenkenan@gmail.com>
2024-09-02 13:48:41 +0200

    Test should now work

--- tapir/coop/tests/test_create_membershipresignation.py
+++ tapir/coop/tests/test_create_membershipresignation.py
@@ -1,3 +1,4 @@
+from icecream import ic
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from django.urls import reverse
@@ -5,8 +6,10 @@ from django.urls import reverse
 from tapir.coop.tests.factories import (
     MembershipResignationFactory,
     ShareOwnerFactory,
-    MembershipPauseFactory,
 )
+from tapir.core.models import FeatureFlag
+from tapir.coop.config import feature_flag_membership_resignation
+
 from tapir.utils.tests_utils import TapirFactoryTestBase
 
 
@@ -14,17 +17,21 @@ class TestCreateMembershipResignation(TapirFactoryTestBase):
     def test_membershipResignation_created_isTrue(self):
         self.login_as_member_office_user()
         resignation = MembershipResignationFactory.build()
+        resignation_flag = FeatureFlag.objects.create(
+            flag_name=feature_flag_membership_resignation, flag_value=True
+        )
+        resignation_flag.save()
 
         response = self.client.post(
             reverse("coop:resign_new_membership"),
             {
-                "share_owner": resignation.share_owner,
+                "share_owner": ShareOwnerFactory.create().id,
                 "cancellation_date": datetime.now(),
                 "pay_out_day": datetime.now()
                 + relativedelta(day=31, month=12, years=3),
                 "cancellation_reason": resignation.cancellation_reason,
                 "resignation_type": resignation.resignation_type,
-                "transferring_shares_to": resignation.transferring_shares_to,
+                "transferring_shares_to": ShareOwnerFactory.create().id,
                 "paid_out": resignation.paid_out,
             },
             follow=True,
