4791b4995ab0bda71f5cdd7b009cc135eeb91261
Théophile Madet <theo.madet@posteo.net>
2024-09-27 14:45:11 +0200

    Temporary test.

--- tapir/statistics/views/private_views.py
+++ tapir/statistics/views/private_views.py
@@ -134,10 +134,11 @@ class ShiftCountByCategoryJsonView(
             slot__shift__cancelled=False,
         )
 
-        # Only pick one attendance per slot, choosing the most recently updated one
-        attendances = attendances.order_by("slot", "-last_state_update").distinct(
-            "slot"
-        )
+        if False:
+            # Only pick one attendance per slot, choosing the most recently updated one
+            attendances = attendances.order_by("slot", "-last_state_update").distinct(
+                "slot"
+            )
 
         if selection == cls.SELECTION_ABCD_MEMBERS:
             attendances = cls.filter_attendance_by_attendance_mode_of_member_at_date(
