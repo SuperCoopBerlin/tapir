aca176dd9c7216c73fe92a6766b118f1fd8ebf98
Théophile Madet <theo.madet@posteo.net>
2024-10-18 17:43:08 +0200

    Added dynamic API root instead of hardcoded localhost.

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,12 +1,13 @@
 import { Configuration } from "../api-client";
 import { getCookie } from "../utils/getCookie";
+import env from "../env.ts";
 
 export function useApi<T>(
   ApiClient: new (configuration: Configuration) => T,
 ): T {
   return new ApiClient(
     new Configuration({
-      basePath: "http://localhost:8000",
+      basePath: env.REACT_APP_API_ROOT,
       headers: { "X-CSRFToken": getCookie("csrftoken") },
     }),
   );
