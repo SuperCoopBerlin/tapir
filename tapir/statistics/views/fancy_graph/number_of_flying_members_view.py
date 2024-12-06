91d5cad649bcf7f2874ec10cd87feea3dd3ab785
Théophile Madet <theo.madet@posteo.net>
2024-12-06 20:12:52 +0100

    Removed get_shift_user_datas_of_working_members_annotated_with_attendance_mode, replacing it with usages of ShiftExpectationService and ShiftAttendanceModeService

--- tapir/statistics/views/fancy_graph/number_of_flying_members_view.py
+++ tapir/statistics/views/fancy_graph/number_of_flying_members_view.py
@@ -1,27 +1,31 @@
 import datetime
 
-from tapir.shifts.models import ShiftAttendanceMode
+from tapir.shifts.models import ShiftAttendanceMode, ShiftUserData
 from tapir.shifts.services.shift_attendance_mode_service import (
     ShiftAttendanceModeService,
 )
+from tapir.shifts.services.shift_expectation_service import ShiftExpectationService
 from tapir.statistics.views.fancy_graph.base_view import (
     DatapointView,
-    get_shift_user_datas_of_working_members_annotated_with_attendance_mode,
 )
 
 
 class NumberOfFlyingMembersAtDateView(DatapointView):
     def calculate_datapoint(self, reference_time: datetime.datetime) -> int:
-        shift_user_datas = (
-            get_shift_user_datas_of_working_members_annotated_with_attendance_mode(
-                reference_time
-            )
-        )
+        shift_user_datas = ShiftUserData.objects.all()
 
-        shift_user_datas = shift_user_datas.filter(
+        working_members = (
+            ShiftExpectationService.annotate_shift_user_data_queryset_with_working_status_at_datetime(
+                shift_user_datas, reference_time
+            )
+        ).filter(**{ShiftExpectationService.ANNOTATION_IS_WORKING_AT_DATE: True})
+
+        working_and_abcd_members = ShiftAttendanceModeService.annotate_shift_user_data_queryset_with_attendance_mode_at_datetime(
+            working_members, reference_time
+        ).filter(
             **{
                 ShiftAttendanceModeService.ANNOTATION_SHIFT_ATTENDANCE_MODE_AT_DATE: ShiftAttendanceMode.FLYING
             }
         )
 
-        return shift_user_datas.count()
+        return working_and_abcd_members.count()
--- tapir/statistics/views/fancy_graph/number_of_shift_partners_view.py
+++ tapir/statistics/views/fancy_graph/number_of_shift_partners_view.py
@@ -1,19 +1,22 @@
+from tapir.shifts.models import ShiftUserData
+from tapir.shifts.services.shift_expectation_service import ShiftExpectationService
 from tapir.shifts.services.shift_partner_history_service import (
     ShiftPartnerHistoryService,
 )
 from tapir.statistics.views.fancy_graph.base_view import (
     DatapointView,
-    get_shift_user_datas_of_working_members_annotated_with_attendance_mode,
 )
 
 
 class NumberOfShiftPartnersAtDateView(DatapointView):
     def calculate_datapoint(self, reference_time) -> int:
+        shift_user_datas = ShiftUserData.objects.all()
+
         shift_user_datas = (
-            get_shift_user_datas_of_working_members_annotated_with_attendance_mode(
-                reference_time
+            ShiftExpectationService.annotate_shift_user_data_queryset_with_working_status_at_datetime(
+                shift_user_datas, reference_time
             )
-        )
+        ).filter(**{ShiftExpectationService.ANNOTATION_IS_WORKING_AT_DATE: True})
 
         shift_user_datas = ShiftPartnerHistoryService.annotate_shift_user_data_queryset_with_has_shift_partner_at_date(
             shift_user_datas, reference_time
