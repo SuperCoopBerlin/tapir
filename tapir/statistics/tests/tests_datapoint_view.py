579d8ca168185617981cdc631bbe81000429ca48
Théophile Madet <theo.madet@posteo.net>
2024-12-06 17:14:34 +0100

    Added tests around DatapointView.get

--- tapir/statistics/tests/tests_datapoint_view.py
+++ tapir/statistics/tests/tests_datapoint_view.py
@@ -1,14 +1,23 @@
 import datetime
 from unittest.mock import patch, Mock
 
+from django.test import RequestFactory
+
+from tapir.accounts.tests.factories.factories import TapirUserFactory
 from tapir.statistics.models import FancyGraphCache
 from tapir.statistics.views.fancy_graph.base_view import DatapointView
 from tapir.utils.tests_utils import TapirFactoryTestBase, mock_timezone_now
 
 
 class DummyDatapointView(DatapointView):
+    VALUES = {
+        datetime.date(year=2023, month=4, day=5): 10,
+        datetime.date(year=2023, month=4, day=1): 8,
+        datetime.date(year=2023, month=3, day=1): 5,
+    }
+
     def calculate_datapoint(self, reference_time: datetime.datetime) -> int:
-        return 1
+        return self.VALUES[reference_time.date()]
 
 
 class TestDatapointView(TapirFactoryTestBase):
@@ -56,3 +65,36 @@ class TestDatapointView(TapirFactoryTestBase):
 
         mock_calculate_datapoint.assert_called_once_with(self.NOW)
         self.assertEqual(2, result)
+
+    def test_viewGet_notRelative_returnsDatapoint(self):
+        request_factory = RequestFactory()
+        request = request_factory.get(
+            "", query_params={"relative": "false", "at_date": "2023-4-1"}
+        )
+        request.user = TapirUserFactory.create(is_in_member_office=True)
+
+        response = DummyDatapointView.as_view()(request)
+
+        self.assertEqual(8, response.data)
+
+    def test_viewGet_relativeFromToday_returnsDiffFromTodayToStartOfMonth(self):
+        request_factory = RequestFactory()
+        request = request_factory.get(
+            "", query_params={"relative": "true", "at_date": "2023-4-5"}
+        )
+        request.user = TapirUserFactory.create(is_in_member_office=True)
+
+        response = DummyDatapointView.as_view()(request)
+
+        self.assertEqual(2, response.data)
+
+    def test_viewGet_relativeFromFirstOfMonth_returnsDiffFromLastMonth(self):
+        request_factory = RequestFactory()
+        request = request_factory.get(
+            "", query_params={"relative": "true", "at_date": "2023-4-1"}
+        )
+        request.user = TapirUserFactory.create(is_in_member_office=True)
+
+        response = DummyDatapointView.as_view()(request)
+
+        self.assertEqual(3, response.data)
