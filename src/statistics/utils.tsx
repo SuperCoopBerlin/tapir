6ca741b2a3558bd4268d915f6a6f4f27590171a1
Théophile Madet <theo.madet@posteo.net>
2024-12-10 18:59:49 +0100

    Added options to the fancy graph: - add today's date or not - use first of month or last of month

--- src/statistics/utils.tsx
+++ src/statistics/utils.tsx
@@ -2,3 +2,7 @@ export function getFirstOfMonth(date: Date) {
   date.setDate(1);
   return date;
 }
+
+export function getLastOfMonth(date: Date) {
+  return new Date(date.getFullYear(), date.getMonth() + 1, 0);
+}
