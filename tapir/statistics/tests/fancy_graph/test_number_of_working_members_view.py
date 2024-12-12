cea7754292762596ae445daa1bbf87ce3facf2e8
Théophile Madet <theo.madet@posteo.net>
2024-12-12 11:02:58 +0100

    Added tests for NumberOfWorkingMembersAtDateView

---
+++ tapir/statistics/tests/fancy_graph/test_number_of_working_members_view.py
@@ -0,0 +1,43 @@
+import datetime
+
+from django.utils import timezone
+
+from tapir.coop.models import ShareOwner
+from tapir.statistics.views.fancy_graph.number_of_working_members_view import (
+    NumberOfWorkingMembersAtDateView,
+)
+from tapir.utils.tests_utils import (
+    TapirFactoryTestBase,
+    mock_timezone_now,
+    create_member_that_can_shop,
+)
+
+
+class TestNumberOfWorkingMembersView(TapirFactoryTestBase):
+    NOW = datetime.datetime(year=2023, month=4, day=1, hour=12)
+    REFERENCE_TIME = timezone.make_aware(
+        datetime.datetime(year=2022, month=6, day=15, hour=12)
+    )
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.NOW = mock_timezone_now(self, self.NOW)
+
+    def test_calculateDatapoint_memberIsWorking_counted(self):
+        create_member_that_can_shop(self, self.REFERENCE_TIME)
+
+        result = NumberOfWorkingMembersAtDateView().calculate_datapoint(
+            self.REFERENCE_TIME
+        )
+
+        self.assertEqual(1, result)
+
+    def test_calculateDatapoint_memberIsNotWorking_notCounted(self):
+        create_member_that_can_shop(self, self.REFERENCE_TIME)
+        ShareOwner.objects.update(is_investing=True)
+
+        result = NumberOfWorkingMembersAtDateView().calculate_datapoint(
+            self.REFERENCE_TIME
+        )
+
+        self.assertEqual(0, result)
--- tapir/statistics/views/fancy_graph/number_of_working_members_view.py
+++ tapir/statistics/views/fancy_graph/number_of_working_members_view.py
@@ -1,65 +1,18 @@
 import datetime
 
-from tapir.coop.models import ShareOwner
-from tapir.coop.services.investing_status_service import InvestingStatusService
-from tapir.coop.services.membership_pause_service import MembershipPauseService
-from tapir.coop.services.number_of_shares_service import NumberOfSharesService
 from tapir.shifts.models import ShiftUserData
-from tapir.shifts.services.frozen_status_history_service import (
-    FrozenStatusHistoryService,
-)
 from tapir.shifts.services.shift_expectation_service import ShiftExpectationService
 from tapir.statistics.views.fancy_graph.base_view import DatapointView
-from tapir.utils.shortcuts import transfer_attributes
 
 
 class NumberOfWorkingMembersAtDateView(DatapointView):
     def calculate_datapoint(self, reference_time: datetime.datetime) -> int:
-        reference_date = reference_time.date()
+        queryset = ShiftExpectationService.annotate_shift_user_data_queryset_with_working_status_at_datetime(
+            ShiftUserData.objects.all(), reference_time
+        )
 
-        shift_user_datas = (
-            ShiftUserData.objects.filter(user__share_owner__isnull=False)
-            .prefetch_related("user")
-            .prefetch_related("user__share_owner")
-            .prefetch_related("user__share_owner__share_ownerships")
-            .prefetch_related("shift_exemptions")
+        queryset = queryset.filter(
+            **{ShiftExpectationService.ANNOTATION_IS_WORKING_AT_DATE: True}
         )
-        shift_user_datas = FrozenStatusHistoryService.annotate_shift_user_data_queryset_with_is_frozen_at_datetime(
-            shift_user_datas, reference_time
-        ).distinct()
 
-        share_owners = NumberOfSharesService.annotate_share_owner_queryset_with_nb_of_active_shares(
-            ShareOwner.objects.all(), reference_date
-        )
-        share_owners = (
-            MembershipPauseService.annotate_share_owner_queryset_with_has_active_pause(
-                share_owners, reference_date
-            )
-        )
-        share_owners = InvestingStatusService.annotate_share_owner_queryset_with_investing_status_at_datetime(
-            share_owners, reference_time
-        )
-        share_owners = {share_owner.id: share_owner for share_owner in share_owners}
-        for shift_user_data in shift_user_datas:
-            transfer_attributes(
-                share_owners[shift_user_data.user.share_owner.id],
-                shift_user_data.user.share_owner,
-                [
-                    NumberOfSharesService.ANNOTATION_NUMBER_OF_ACTIVE_SHARES,
-                    NumberOfSharesService.ANNOTATION_SHARES_ACTIVE_AT_DATE,
-                    MembershipPauseService.ANNOTATION_HAS_ACTIVE_PAUSE,
-                    MembershipPauseService.ANNOTATION_HAS_ACTIVE_PAUSE_AT_DATE,
-                    InvestingStatusService.ANNOTATION_WAS_INVESTING,
-                    InvestingStatusService.ANNOTATION_WAS_INVESTING_AT_DATE,
-                ],
-            )
-
-        return len(
-            [
-                shift_user_data
-                for shift_user_data in shift_user_datas
-                if ShiftExpectationService.is_member_expected_to_do_shifts(
-                    shift_user_data, reference_time
-                )
-            ]
-        )
+        return queryset.count()
