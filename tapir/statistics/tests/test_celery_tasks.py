114c72025f48ceed6e444187f7558e6f60859344
Kersten Weise <kerstenkenan@gmail.com>
2024-12-17 12:56:39 +0100

    Removed double brackets.

--- tapir/statistics/tests/test_celery_tasks.py
+++ tapir/statistics/tests/test_celery_tasks.py
@@ -19,10 +19,11 @@ from tapir.statistics.tasks import process_credit_account
 
 
 class TestCeleryTasks(SimpleTestCase):
-    @patch("tapir.statistics.tasks.call_command"))
+    @patch("tapir.statistics.tasks.call_command")
     def test_processPurchaseFiles(self, mock_call_command: Mock):
         process_purchase_files()
         mock_call_command.assert_called_once_with("process_purchase_files")
+
     def test_processCreditAccount(self, mock_call_command: Mock):
         process_credit_account()
         mock_call_command.assert_called_once_with("process_credit_account")
