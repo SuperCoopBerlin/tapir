6eed48ed92c4aab1178692020ab9c56a13b9217e
Kersten Weise <kerstenkenan@gmail.com>
2024-12-17 12:47:09 +0100

    Implementing a scheduled regulary task for credit account.

--- tapir/statistics/tests/test_celery_tasks.py
+++ tapir/statistics/tests/test_celery_tasks.py
@@ -15,10 +15,14 @@ from tapir.shifts.tasks import (
     run_freeze_checks,
 )
 from tapir.statistics.tasks import process_purchase_files
+from tapir.statistics.tasks import process_credit_account
 
 
 class TestCeleryTasks(SimpleTestCase):
-    @patch("tapir.statistics.tasks.call_command")
+    @patch("tapir.statistics.tasks.call_command"))
     def test_processPurchaseFiles(self, mock_call_command: Mock):
         process_purchase_files()
         mock_call_command.assert_called_once_with("process_purchase_files")
+    def test_processCreditAccount(self, mock_call_command: Mock):
+        process_credit_account()
+        mock_call_command.assert_called_once_with("process_credit_account")
