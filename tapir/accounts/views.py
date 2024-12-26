868d8ef6ebd95a3e32ed101d55046a4965f69e62
crosspolar <18083323+crosspolar@users.noreply.github.com>
2024-12-26 16:42:12 +0100

    (renovate) remove unnecessary template

--- tapir/accounts/views.py
+++ tapir/accounts/views.py
@@ -344,6 +344,20 @@ class MailSettingsView(
     template_name = "accounts/mailsettings.html"
     form_class = OptionalMailsForm
 
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data["page_title"] = _("Notification settings for %(name)s") % {
+            "name": UserUtils.build_display_name_for_viewer(
+                self.get_tapir_user().share_owner, self.request.user
+            )
+        }
+        context_data["card_title"] = _("Notification settings for %(name)s") % {
+            "name": UserUtils.build_display_name_for_viewer(
+                self.get_tapir_user().share_owner, self.request.user
+            )
+        }
+        return context_data
+
     def get_permission_required(self):
         if self.request.user.pk == self.get_tapir_user().pk:
             return []
