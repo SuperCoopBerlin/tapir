94561459aad2ddf06cd7d5763e971d4ef3bf189d
crosspolar <18083323+crosspolar@users.noreply.github.com>
2024-12-27 14:30:49 +0100

    tests

--- tapir/core/tests/test_email_preferences.py
+++ tapir/core/tests/test_email_preferences.py
@@ -1,8 +1,9 @@
 from tapir.accounts.models import TapirUser, OptionalMails, MailChoice
 from tapir.accounts.tests.factories.factories import TapirUserFactory
-from tapir.coop.emails.tapir_account_created_email import TapirAccountCreatedEmail
 from tapir.core.tapir_email_base import TapirEmailBase
 from tapir.utils.tests_utils import TapirFactoryTestBase
+
+from django.urls import reverse
 
 
 class OptionalNonDefaultMail(TapirEmailBase):
@@ -55,3 +56,49 @@ class TestOptionalNotDefaultMail(TapirFactoryTestBase):
                 user=tapir_user
             )
         )
+
+
+class TestMailSetting(TapirFactoryTestBase):
+    def setUp(self):
+        TapirEmailBase.register_email(OptionalNonDefaultMail)
+
+    def test_normal_user_cant_access_mail_setting(self):
+        tapir_user = TapirUserFactory()
+        actor = TapirUserFactory()
+        self.login_as_user(actor)
+        response = self.client.get(
+            reverse("accounts:mail_settings", args=[tapir_user.pk]),
+        )
+
+        self.assertEqual(403, response.status_code)
+        # tapir_user.refresh_from_db()
+        # self.assertEqual(False, tapir_user.allows_purchase_tracking)
+
+    def test_normal_user_cannot_update_other_users_mail_setting(self):
+        tapir_user = TapirUserFactory()
+        self.assertFalse(
+            OptionalNonDefaultMail().user_wants_to_or_has_to_receive_mail(
+                user=tapir_user
+            )
+        )
+
+        actor = TapirUserFactory()
+        self.login_as_user(actor)
+        self.client.post(
+            reverse("accounts:mail_settings", args=[tapir_user.pk]),
+            {"optional_mails": OptionalNonDefaultMail.get_unique_id()},
+            follow=True,
+        )
+        self.assertFalse(
+            OptionalNonDefaultMail().user_wants_to_or_has_to_receive_mail(
+                user=tapir_user
+            )
+        )
+
+    def test_user_can_access_own_settings(self):
+        tapir_user = TapirUserFactory()
+        self.login_as_user(tapir_user)
+        response = self.client.get(
+            reverse("accounts:mail_settings", args=[tapir_user.pk]),
+        )
+        self.assertTrue(200, response.status_code)
