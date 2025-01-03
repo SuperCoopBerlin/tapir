9a4e39e8d7aca71f2aa9b593bec67e6d2398a7d7
Théophile Madet <theo.madet@posteo.net>
2025-01-03 11:36:01 +0100

    WIP more tests for SelfUnregisterService

--- tapir/shifts/tests/test_self_unregister_service.py
+++ tapir/shifts/tests/test_self_unregister_service.py
@@ -1,4 +1,5 @@
 import datetime
+from unittest.mock import Mock
 
 from tapir.accounts.tests.factories.factories import TapirUserFactory
 from tapir.shifts.models import (
@@ -9,10 +10,15 @@ from tapir.shifts.models import (
 )
 from tapir.shifts.services.self_unregister_service import SelfUnregisterService
 from tapir.shifts.tests.factories import ShiftFactory, ShiftTemplateFactory
-from tapir.utils.tests_utils import TapirFactoryTestBase
+from tapir.utils.tests_utils import TapirFactoryTestBase, mock_timezone_now
 
 
 class TestSelfUnregisterService(TapirFactoryTestBase):
+    NOW = datetime.datetime(year=2024, month=3, day=4)
+
+    def setUp(self) -> None:
+        self.NOW = mock_timezone_now(self, self.NOW)
+
     def test_shouldShowIsAbcdAttendanceReason_shiftIsNotAbcd_returnsFalse(self):
         tapir_user = TapirUserFactory.create()
         ShiftFactory.create()
@@ -55,5 +61,29 @@ class TestSelfUnregisterService(TapirFactoryTestBase):
         self.assertTrue(
             SelfUnregisterService.should_show_is_abcd_attendance_reason(
                 user=tapir_user, attendance=ShiftAttendance.objects.get()
+            )
+        )
+
+    def test_shouldShowNotEnoughDaysBeforeShiftReason_dateIsNotEnoughDaysBeforeShift_returnsTrue(
+        self,
+    ):
+        attendance = Mock()
+        attendance.slot.shift.start_time = self.NOW + datetime.timedelta(days=6)
+
+        self.assertTrue(
+            SelfUnregisterService.should_show_not_enough_days_before_shift_reason(
+                attendance=attendance
+            )
+        )
+
+    def test_shouldShowNotEnoughDaysBeforeShiftReason_dateIsEnoughDaysBeforeShift_returnsFalse(
+        self,
+    ):
+        attendance = Mock()
+        attendance.slot.shift.start_time = self.NOW + datetime.timedelta(days=8)
+
+        self.assertFalse(
+            SelfUnregisterService.should_show_not_enough_days_before_shift_reason(
+                attendance=attendance
             )
         )
