24c127d4fcb3693e68bde5668b9f642655a169da
Théophile Madet <theo.madet@posteo.net>
2025-01-06 17:53:25 +0100

    Added tests for shifts:attendance_cant_attend

--- tapir/coop/tests/membership_resignation/test_create_view.py
+++ tapir/coop/tests/membership_resignation/test_create_view.py
@@ -44,14 +44,14 @@ class TestMembershipResignationCreateView(
         self.given_feature_flag_value(feature_flag_membership_resignation, True)
         mock_timezone_now(self, self.NOW)
 
-    def get_allowed_groups(self):
+    def permission_test_get_allowed_groups(self):
         return [
             settings.GROUP_VORSTAND,
             settings.GROUP_EMPLOYEES,
             settings.GROUP_MEMBER_OFFICE,
         ]
 
-    def do_request(self):
+    def permission_test_do_request(self):
         return self.client.get(reverse("coop:membership_resignation_create"))
 
     def test_membershipResignationCreateView_featureFlagDisabled_accessDenied(self):
