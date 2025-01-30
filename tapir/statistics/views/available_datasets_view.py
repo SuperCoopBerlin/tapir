4e7b6133a7b21a73241e54c78ee701f627afef65
Théophile Madet <theo.madet@posteo.net>
2025-01-30 17:25:08 +0100

    Made random colors consistent

--- tapir/statistics/views/available_datasets_view.py
+++ tapir/statistics/views/available_datasets_view.py
@@ -1,14 +1,12 @@
 from distinctipy import distinctipy
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from drf_spectacular.utils import extend_schema
-from icecream import ic
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from tapir.settings import PERMISSION_COOP_MANAGE
 from tapir.statistics.serializers import DatasetSerializer
-from tapir.statistics.services.data_providers import *  # noqa
 from tapir.statistics.services.data_providers.base_data_provider import data_providers
 
 
@@ -39,9 +37,8 @@ class AvailableDatasetsView(LoginRequiredMixin, PermissionRequiredMixin, APIView
             }
             for provider_id, provider in data_providers.items()
         ]
-        ic(data_providers.items())
 
-        colors = distinctipy.get_colors(len(datasets))
+        colors = distinctipy.get_colors(len(datasets), rng=123456)
         for index, dataset in enumerate(datasets):
             dataset["color"] = colors[index]
             dataset["point_style"] = self.POINT_STYLES[index % len(self.POINT_STYLES)]
