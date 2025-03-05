e109cf3cc9b941fe539b15a27253dca992db969b
Kersten Weise <kerstenkenan@gmail.com>
2025-03-05 20:40:00 +0100

    Fixed tests for ShitTemplateDuplicateView incl. new titles

--- tapir/shifts/tests/test_shift_template_duplicate.py
+++ tapir/shifts/tests/test_shift_template_duplicate.py
@@ -9,13 +9,12 @@ from tapir.shifts.models import ShiftTemplate, ShiftTemplateGroup
 
 
 class TestShiftTemplateDuplicate(TapirFactoryTestBase):
-    def test_shiftTemplateDuplicateForm_duplicateSingleTemplate(self):
+    def test_shiftTemplateDuplicateView_duplicateSingleTemplate_allFieldsGetsCopied(
+        self,
+    ):
         self.login_as_member_office_user()
         ShiftTemplateGroup.objects.bulk_create(
-            [
-                ShiftTemplateGroup(name="A"),
-                ShiftTemplateGroup(name="B"),
-            ]
+            [ShiftTemplateGroup(name="A"), ShiftTemplateGroup(name="B")]
         )
         shift_template_source = ShiftTemplateFactory.create(
             start_date=datetime.date.today() + datetime.timedelta(days=1),
@@ -24,11 +23,8 @@ class TestShiftTemplateDuplicate(TapirFactoryTestBase):
         )
 
         response = self.client.post(
-            reverse("shifts:shift_template_duplicate", args=[shift_template_source.pk]),
+            reverse("shifts:shift_template_duplicate", args=[shift_template_source.id]),
             {
-                "start_date": shift_template_source.start_date,
-                "start_time": shift_template_source.start_time,
-                "end_time": shift_template_source.end_time,
                 "week_group": 1,
                 "weekdays": 3,
             },
@@ -36,7 +32,9 @@ class TestShiftTemplateDuplicate(TapirFactoryTestBase):
         )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(2, ShiftTemplate.objects.count())
-        shift_template_duplicate = ShiftTemplate.objects.all()[1]
+        shift_template_duplicate = ShiftTemplate.objects.exclude(
+            id=shift_template_source.id
+        ).get()
         self.assertEqual(shift_template_duplicate.name, shift_template_source.name)
         self.assertEqual(
             shift_template_duplicate.description, shift_template_source.description
@@ -46,36 +44,29 @@ class TestShiftTemplateDuplicate(TapirFactoryTestBase):
         )
         self.assertEqual(
             shift_template_source.num_required_attendances,
-            shift_template_source.num_required_attendances,
+            shift_template_duplicate.num_required_attendances,
         )
         self.assertEqual(
-            shift_template_duplicate.start_date,
             shift_template_source.start_date,
+            shift_template_duplicate.start_date,
         )
         self.assertEqual(
-            shift_template_duplicate.start_time, shift_template_source.start_time
+            shift_template_source.start_time, shift_template_duplicate.start_time
         )
         self.assertEqual(
-            shift_template_duplicate.end_time, shift_template_source.end_time
+            shift_template_source.end_time,
+            shift_template_duplicate.end_time,
         )
-        for duplicate_template, source_template in product(
-            shift_template_duplicate.slot_templates.all(),
-            shift_template_source.slot_templates.all(),
-        ):
-            self.assertEqual(duplicate_template.name, source_template.name)
-            self.assertEqual(
-                duplicate_template.required_capabilities,
-                source_template.required_capabilities,
-            )
+        self.assertEqual(
+            shift_template_source.slot_templates.values("name")[0],
+            shift_template_duplicate.slot_templates.values("name")[0],
+        )
 
-    def test_shiftTemplateDuplicateForm_originalTemplateNotDuplicated(self):
+    def test_shiftTemplateDuplicateView_duplicateSingleTemplate_originalTemplateGetsNotDuplicated(
+        self,
+    ):
         self.login_as_member_office_user()
-        ShiftTemplateGroup.objects.bulk_create(
-            [
-                ShiftTemplateGroup(name="A"),
-                ShiftTemplateGroup(name="B"),
-            ]
-        )
+        ShiftTemplateGroup.objects.create(name="A")
         shift_template = ShiftTemplateFactory.create(
             start_date=datetime.datetime.now() + datetime.timedelta(days=1),
             group=ShiftTemplateGroup.objects.get(name="A"),
@@ -83,7 +74,7 @@ class TestShiftTemplateDuplicate(TapirFactoryTestBase):
         )
 
         response = self.client.post(
-            reverse("shifts:shift_template_duplicate", args=[shift_template.pk]),
+            reverse("shifts:shift_template_duplicate", args=[shift_template.id]),
             {
                 "start_date": shift_template.start_date,
                 "start_time": shift_template.start_time,
