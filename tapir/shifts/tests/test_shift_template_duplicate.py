419649753210ef16479c8f5ee64eb48db73d61be
Kersten Weise <kerstenkenan@gmail.com>
2025-03-19 17:00:48 +0100

    Improved shift-duplication-test regarding slot templates

--- tapir/shifts/tests/test_shift_template_duplicate.py
+++ tapir/shifts/tests/test_shift_template_duplicate.py
@@ -30,7 +30,7 @@ class TestShiftTemplateDuplicate(TapirFactoryTestBase):
             },
             follow=True,
         )
-        self.assertEqual(response.status_code, 200)
+        self.assertStatusCode(response, 200)
         self.assertEqual(2, ShiftTemplate.objects.count())
         shift_template_duplicate = ShiftTemplate.objects.exclude(
             id=shift_template_source.id
@@ -58,9 +58,17 @@ class TestShiftTemplateDuplicate(TapirFactoryTestBase):
             shift_template_duplicate.end_time,
         )
         self.assertEqual(
-            shift_template_source.slot_templates.values("name")[0],
-            shift_template_duplicate.slot_templates.values("name")[0],
+            shift_template_source.slot_templates.count(),
+            shift_template_duplicate.slot_templates.count(),
         )
+        for slot_template_source, slot_template_duplicate in product(
+            shift_template_source.slot_templates.all(),
+            shift_template_duplicate.slot_templates.all(),
+        ):
+            self.assertEqual(
+                slot_template_source.name,
+                slot_template_duplicate.name,
+            )
 
     def test_shiftTemplateDuplicateView_duplicateSingleTemplate_originalTemplateGetsNotDuplicated(
         self,
@@ -76,13 +84,10 @@ class TestShiftTemplateDuplicate(TapirFactoryTestBase):
         response = self.client.post(
             reverse("shifts:shift_template_duplicate", args=[shift_template.id]),
             {
-                "start_date": shift_template.start_date,
-                "start_time": shift_template.start_time,
-                "end_time": shift_template.end_time,
                 "week_group": 0,
                 "weekdays": 3,
             },
             follow=True,
         )
-        self.assertEqual(response.status_code, 200)
+        self.assertStatusCode(response, 200)
         self.assertEqual(1, ShiftTemplate.objects.count())
