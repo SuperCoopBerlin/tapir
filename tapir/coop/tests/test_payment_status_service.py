e851117bc335970fd6e23078fc9c297ffcf8ab0d
Théophile Madet <theo.madet@posteo.net>
2025-03-21 09:49:01 +0100

    Fixed interactions between NumberOfSharesService and PaymentStatusService

--- tapir/coop/tests/test_payment_status_service.py
+++ tapir/coop/tests/test_payment_status_service.py
@@ -1,6 +1,7 @@
 import datetime
 
 from tapir.coop.models import ShareOwner, ShareOwnership
+from tapir.coop.services.number_of_shares_service import NumberOfSharesService
 from tapir.coop.services.payment_status_service import PaymentStatusService
 from tapir.coop.tests.factories import ShareOwnerFactory
 from tapir.coop.tests.incoming_payment_factory import IncomingPaymentFactory
@@ -21,6 +22,33 @@ class TestPaymentStatusService(TapirFactoryTestBase):
         for _ in range(3):
             share_owner = ShareOwnerFactory.create(nb_shares=10)
             IncomingPaymentFactory.create_batch(3, credited_member=share_owner)
+
+    def test_annotateWithPaymentsAtDate_default_returnsCorrectValue(
+        self,
+    ):
+        share_owner = ShareOwnerFactory.create()
+        IncomingPaymentFactory.create(
+            credited_member=share_owner,
+            amount=123,
+            payment_date=datetime.date(year=2023, month=6, day=2),
+        )
+        IncomingPaymentFactory.create(
+            credited_member=share_owner,
+            amount=123,
+            payment_date=datetime.date(year=2023, month=7, day=1),
+        )
+
+        annotated = PaymentStatusService.annotate_with_payments_at_date(
+            ShareOwner.objects.all(), datetime.date(year=2023, month=8, day=15)
+        )
+
+        self.assertEqual(
+            123 + 123,
+            getattr(
+                annotated.get(id=share_owner.id),
+                PaymentStatusService.ANNOTATION_CREDITED_PAYMENTS_SUM_AT_DATE,
+            ),
+        )
 
     def test_annotateWithPaymentsAtDate_somePaymentsHappenedAfterTheDate_onlyPaymentsBeforeTheDateCounted(
         self,
@@ -102,3 +130,41 @@ class TestPaymentStatusService(TapirFactoryTestBase):
                 PaymentStatusService.ANNOTATION_EXPECTED_PAYMENTS_SUM_AT_DATE,
             ),
         )
+
+    def test_annotateWithPaymentsAtDate_alsoAnnotatedWithNumberOfShares_returnsCorrectValue(
+        self,
+    ):
+        # if the NumberOfSharesService doesn't use subqueries, the annotations from PaymentStatusService can return wrong values
+        share_owner = ShareOwnerFactory.create()
+        IncomingPaymentFactory.create(
+            credited_member=share_owner,
+            amount=123,
+            payment_date=datetime.date(year=2023, month=6, day=2),
+        )
+        IncomingPaymentFactory.create(
+            credited_member=share_owner,
+            amount=456,
+            payment_date=datetime.date(year=2023, month=7, day=1),
+        )
+
+        queryset = ShareOwner.objects.all()
+        at_date = datetime.date(year=2023, month=8, day=15)
+        queryset = NumberOfSharesService.annotate_share_owner_queryset_with_nb_of_active_shares(
+            queryset, at_date
+        )
+        queryset = PaymentStatusService.annotate_with_payments_at_date(
+            queryset, at_date
+        )
+
+        for share_owner in queryset.all():
+            manual_sum = 0
+            for incoming_payment in share_owner.credited_payments.all():
+                if incoming_payment.payment_date <= at_date:
+                    manual_sum += incoming_payment.amount
+            self.assertEqual(
+                manual_sum,
+                getattr(
+                    share_owner,
+                    PaymentStatusService.ANNOTATION_CREDITED_PAYMENTS_SUM_AT_DATE,
+                ),
+            )
