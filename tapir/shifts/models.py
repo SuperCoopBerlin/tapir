5531d90c10f7dc34a4c82b63729a8850ea858b38
crosspolar <18083323+crosspolar@users.noreply.github.com>
2025-08-12 18:35:55 +0200

    set default of num_required_attendances to zero

--- tapir/shifts/models.py
+++ tapir/shifts/models.py
@@ -173,7 +173,7 @@ class ShiftTemplate(models.Model):
         on_delete=models.PROTECT,
     )
     num_required_attendances = models.PositiveIntegerField(
-        null=False, blank=False, default=3
+        null=False, blank=False, default=0
     )
     weekday = models.IntegerField(blank=True, null=True, choices=WEEKDAY_CHOICES)
     start_time = models.TimeField(blank=False)
@@ -507,7 +507,7 @@ class Shift(models.Model):
         ),
         null=True,
         blank=False,
-        default=3,
+        default=0,
     )
     description = models.TextField(
         verbose_name=_("Description"),
