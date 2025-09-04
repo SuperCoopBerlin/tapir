f586572d9f2be0d9880b0500327018adb5eaf111
Théophile Madet <theo.madet@posteo.net>
2025-09-04 14:30:39 +0200

    fix: Celery workers now use the environment variables from the .env file.

--- tapir/celery.py
+++ tapir/celery.py
@@ -1,5 +1,7 @@
 import os
+from pathlib import Path
 
+import environ
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
@@ -15,3 +17,10 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+env = environ.Env()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
