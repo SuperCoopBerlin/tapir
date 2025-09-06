235bc9ab4fc7e6d381c40ef7202f4c37422d318b
crosspolar <18083323+crosspolar@users.noreply.github.com>
2025-09-06 11:09:16 +0200

    assign as initial what has been chosen the last time

--- tapir/shifts/forms.py
+++ tapir/shifts/forms.py
@@ -680,5 +680,17 @@ class ShiftWatchForm(forms.ModelForm):
         label=_("Which shift-events do you want to subscribe to?"),
         widget=CheckboxSelectMultiple(),
         disabled=False,
-        initial=get_staffingstatus_defaults,
     )
+
+    def __init__(self, *args, **kwargs):
+        request_user: TapirUser = kwargs.pop("request_user")
+        super().__init__(*args, **kwargs)
+
+        if request_user:
+            last_shiftwatch = (
+                ShiftWatch.objects.filter(user=request_user).order_by("-id").first()
+            )
+            if last_shiftwatch:
+                self.initial["staffing_status"] = last_shiftwatch.staffing_status
+            else:
+                self.initial["staffing_status"] = get_staffingstatus_defaults()
