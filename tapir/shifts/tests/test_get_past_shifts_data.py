ef7ac3d0b5f861695844b8e94dc205178c9d856a
crosspolar <18083323+crosspolar@users.noreply.github.com>
2025-09-09 16:36:06 +0200

    refactor tests

--- tapir/shifts/tests/test_get_past_shifts_data.py
+++ tapir/shifts/tests/test_get_past_shifts_data.py
@@ -14,7 +14,7 @@ from tapir.utils.tests_utils import TapirFactoryTestBase
 
 class ShiftGetPastShiftsStatisticsTests(TapirFactoryTestBase):
 
-    def test_getPastShiftsData_DifferentStatesOfAttendandance_onlyCountDoneState(self):
+    def test_getPastShiftsData_differentStatesOfAttendandance_onlyCountDoneState(self):
         user_done = TapirUserFactory.create()
         user_excused = TapirUserFactory.create()
 
@@ -42,8 +42,8 @@ class ShiftGetPastShiftsStatisticsTests(TapirFactoryTestBase):
             (shift.end_time - shift.start_time).total_seconds() / 3600,
         )
 
-    def test_getPastShiftsData_MultipleAttendandances_correctValues(self):
-        users = [TapirUserFactory.create() for _ in range(5)]
+    def test_getPastShiftsData_multipleAttendandances_correctValues(self):
+        users = TapirUserFactory.create_batch(5)
         shift_template = ShiftTemplateFactory.create(nb_slots=len(users))
         shifts = [
             ShiftFactory(
@@ -72,23 +72,3 @@ class ShiftGetPastShiftsStatisticsTests(TapirFactoryTestBase):
                 for shift in shifts
             ),
         )
-
-    # def test_no_past_shifts(self):
-    #     # Create a future shift
-    #     future_shift = ShiftFactory.create(
-    #         start_time=self.REFERENCE_TIME + datetime.timedelta(days=1),
-    #         end_time=self.REFERENCE_TIME + datetime.timedelta(days=2),
-    #         shift_template=self.shift_template
-    #     )
-    #
-    #     # Create an instance of the view
-    #     view = ShiftDetailView()
-    #     context = {}
-    #
-    #     # Call the method to test
-    #     view.get_past_shifts_data(future_shift, context)
-    #
-    #     # Assertions
-    #     self.assertEqual(context["no_of_past_shifts"], 0)
-    #     self.assertNotIn("total_valid_attendances", context)
-    #     self.assertNotIn("total_hours", context)
