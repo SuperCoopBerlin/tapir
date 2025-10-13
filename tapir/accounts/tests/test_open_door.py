76de19b10738cdd05d859ad0f5b5fb2835272d89
vincent <jan-vincent.wyszynski@charite.de>
2025-10-13 21:19:46 +0200

    rename tests to comply with naming conventions

--- tapir/accounts/tests/test_open_door.py
+++ tapir/accounts/tests/test_open_door.py
@@ -1,14 +1,16 @@
+from django.core.cache import cache
 from django.urls import reverse
 
 from tapir.accounts.config import feature_flag_open_door
 from tapir.accounts.tests.factories.factories import TapirUserFactory
+from tapir.accounts.config import cache_key_open_door
 from tapir.utils.tests_utils import TapirFactoryTestBase, FeatureFlagTestMixin
 
 
 class TestOpenDoorFeature(FeatureFlagTestMixin, TapirFactoryTestBase):
     """Test the door opening feature with feature flag support."""
 
-    def test_open_door_action_with_feature_enabled(self):
+    def test_openDoor_postWithFeatureEnabledAndAuthenticated_returnsOk(self):
         """Test that door opening works when feature flag is enabled."""
         self.given_feature_flag_value(feature_flag_open_door, True)
         
@@ -20,7 +22,7 @@ class TestOpenDoorFeature(FeatureFlagTestMixin, TapirFactoryTestBase):
         )
         self.assertEqual(200, response.status_code)
 
-    def test_open_door_action_with_feature_disabled(self):
+    def test_openDoor_postWithFeatureDisabledAndAuthenticated_returnsForbidden(self):
         """Test that door opening is blocked when feature flag is disabled."""
         self.given_feature_flag_value(feature_flag_open_door, False)
         
@@ -32,7 +34,7 @@ class TestOpenDoorFeature(FeatureFlagTestMixin, TapirFactoryTestBase):
         )
         self.assertEqual(403, response.status_code)
 
-    def test_get_open_door_status_with_feature_enabled(self):
+    def test_openDoor_getWithFeatureEnabledAndNotTriggered_returnsForbidden(self):
         """Test that status endpoint works when feature flag is enabled."""
         self.given_feature_flag_value(feature_flag_open_door, True)
         
@@ -40,14 +42,26 @@ class TestOpenDoorFeature(FeatureFlagTestMixin, TapirFactoryTestBase):
         response = self.client.get(reverse("accounts:open_door"))
         self.assertEqual(403, response.status_code)
 
-    def test_get_open_door_status_with_feature_disabled(self):
+    def test_openDoor_getWithFeatureEnabledAndTriggered_returnsOk(self):
+        """Test that status endpoint works when feature flag is enabled."""
+        self.given_feature_flag_value(feature_flag_open_door, True)
+
+        user = TapirUserFactory.create()
+        self.login_as_user(user)
+        
+        cache.set(cache_key_open_door, True)
+        
+        response = self.client.get(reverse("accounts:open_door"))
+        self.assertEqual(200, response.status_code)
+
+    def test_openDoor_getWithFeatureDisabled_returnsForbidden(self):
         """Test that status endpoint returns 403 when feature flag is disabled."""
         self.given_feature_flag_value(feature_flag_open_door, False)
         
         response = self.client.get(reverse("accounts:open_door"))
         self.assertEqual(403, response.status_code)
 
-    def test_open_door_requires_authentication(self):
+    def test_openDoor_postRequiresAuthentication_returnsRedirect(self):
         """Test that door opening requires authentication."""
         self.given_feature_flag_value(feature_flag_open_door, True)
         
