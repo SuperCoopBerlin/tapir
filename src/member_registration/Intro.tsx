54b1b3593dcf88ecfab985030b2395b8844b1849
jakubfiala <j4kub.fiala@gmail.com>
2025-10-25 21:01:37 +0200

    add link to login page

--- src/member_registration/Intro.tsx
+++ src/member_registration/Intro.tsx
@@ -2,10 +2,10 @@ declare let gettext: (english_text: string) => string;
 
 export default function Intro() {
   return (
-    <div className="mb-4">
+    <div className="mb-4" style={{ width: "100%", maxWidth: "700px" }}>
       <p>
         <img
-          style={{ width: "100%", maxWidth: "800px" }}
+          style={{ width: "100%" }}
           src="https://supercoop.de/wp-content/uploads/supercoop-header.jpg"
         />
       </p>
