d8b5ec02809c33bc2346cedf00be35b20e700394
crosspolar <18083323+crosspolar@users.noreply.github.com>
2025-11-14 12:32:06 +0100

    test: test_createShiftRecurringWatchTemplate_weekdayAndShifTemplateGroup_entryCreated

---
+++ tapir/shifts/tests/test_CreateWatchRecurringShiftsView.py
@@ -0,0 +1,35 @@
+from django.urls import reverse
+from tapir.accounts.tests.factories.factories import TapirUserFactory
+from tapir.shifts.models import ShiftRecurringWatchTemplate, StaffingStatusChoices
+from tapir.utils.tests_utils import TapirFactoryTestBase
+
+
+class TestCreateWatchRecurringShiftsView(TapirFactoryTestBase):
+    VIEW_NAME = "shifts:create_watch_recurring_shifts"
+
+    def test_createShiftRecurringWatchTemplate_weekdayAndShifTemplateGroup_entryCreated(
+        self,
+    ):
+        self.login_as_member_office_user(preferred_language="en")
+        tapir_user = TapirUserFactory.create()
+
+        form_data = {
+            "shift_templates": [],
+            "weekdays": [1, 2],
+            "shift_template_group": ["A"],
+            "staffing_status": [StaffingStatusChoices.UNDERSTAFFED],
+        }
+
+        response = self.client.post(
+            reverse(self.VIEW_NAME, args=[tapir_user.id]), data=form_data
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(ShiftRecurringWatchTemplate.objects.count(), 1)
+        created_template = ShiftRecurringWatchTemplate.objects.first()
+
+        self.assertEqual(set(created_template.weekdays), {1, 2})
+        self.assertEqual(set(created_template.shift_template_group), {"A"})
+        self.assertEqual(
+            set(created_template.staffing_status), {StaffingStatusChoices.UNDERSTAFFED}
+        )
