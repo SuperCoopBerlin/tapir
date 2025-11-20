1676bdca836138f59b50d9603a4a85667b6e4a8d
crosspolar <18083323+crosspolar@users.noreply.github.com>
2025-11-20 09:26:03 +0100

    test_shareOwnerGetsDeleted_deletedAt_hasDate

--- tapir/coop/tests/test_shareowner_delete_view.py
+++ tapir/coop/tests/test_shareowner_delete_view.py
@@ -1,7 +1,10 @@
+from http import HTTPStatus
+
 from django.urls import reverse
 
 from tapir import settings
 from tapir.accounts.tests.factories.factories import TapirUserFactory
+from tapir.coop.models import DeleteShareOwnerLogEntry
 from tapir.utils.tests_utils import (
     PermissionTestMixin,
     FeatureFlagTestMixin,
@@ -22,8 +25,20 @@ class TestShareOwnerDeleteView(
         ]
 
     def do_request(self):
-        tapir_user = TapirUserFactory(allows_purchase_tracking=True)
+        tapir_user = TapirUserFactory()
         return self.client.post(
             reverse("coop:shareowner_delete", args=[tapir_user.share_owner.id]),
             follow=True,
         )
+
+    def test_shareOwnerGetsDeleted_deletedAt_hasDate(self):
+        self.login_as_vorstand()
+        tapir_user = TapirUserFactory()
+        response = self.client.post(
+            reverse("coop:shareowner_delete", args=[tapir_user.share_owner.id]),
+            follow=True,
+        )
+        self.assertStatusCode(response, HTTPStatus.OK)
+        print(tapir_user.share_owner.deleted_at)
+        self.assertIsNotNone(tapir_user.share_owner.deleted_at)
+        self.assertEqual(DeleteShareOwnerLogEntry.objects.count(), 1)
