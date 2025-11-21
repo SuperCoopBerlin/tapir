a6349a173c363f8e1d6f758aa18d10896f4211f6
crosspolar <18083323+crosspolar@users.noreply.github.com>
2025-11-21 16:10:56 +0100

    tests users cannot delete itself

--- tapir/coop/tests/test_shareowner_delete_view.py
+++ tapir/coop/tests/test_shareowner_delete_view.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 from tapir import settings
 from tapir.accounts.tests.factories.factories import TapirUserFactory
-from tapir.coop.models import DeleteShareOwnerLogEntry
+from tapir.coop.models import DeleteShareOwnerLogEntry, ShareOwner
 from tapir.utils.tests_utils import (
     PermissionTestMixin,
     FeatureFlagTestMixin,
@@ -31,7 +31,7 @@ class TestShareOwnerDeleteView(
             follow=True,
         )
 
-    def test_shareOwnerGetsDeleted_deletedAt_hasDate(self):
+    def test_ShareOwnerDeleteView_shareOwnerdeletedAt_hasDate(self):
         self.login_as_vorstand()
         tapir_user = TapirUserFactory()
         response = self.client.post(
@@ -40,6 +40,27 @@ class TestShareOwnerDeleteView(
         )
         self.assertStatusCode(response, HTTPStatus.OK)
         tapir_user.share_owner.refresh_from_db()
-        print(tapir_user.share_owner.deleted_at)
         self.assertIsNotNone(tapir_user.share_owner.deleted_at)
         self.assertEqual(DeleteShareOwnerLogEntry.objects.count(), 1)
+
+    def test_ShareOwnerDeleteView_normalShareOwner_cannotDeleteItself(self):
+        tapir_user = self.login_as_normal_user()
+        response = self.client.post(
+            reverse("coop:shareowner_delete", args=[tapir_user.share_owner.id]),
+            follow=True,
+        )
+        self.assertStatusCode(response, HTTPStatus.FORBIDDEN)
+        tapir_user.share_owner.refresh_from_db()
+        self.assertIsNone(tapir_user.share_owner.deleted_at)
+        self.assertEqual(DeleteShareOwnerLogEntry.objects.count(), 0)
+
+    def test_ShareOwnerDeleteView_vorstandShareOwner_cannotDeleteItself(self):
+        tapir_user = self.login_as_vorstand()
+        response = self.client.post(
+            reverse("coop:shareowner_delete", args=[tapir_user.share_owner.id]),
+            follow=True,
+        )
+        self.assertStatusCode(response, HTTPStatus.FORBIDDEN)
+        tapir_user.share_owner.refresh_from_db()
+        self.assertIsNone(tapir_user.share_owner.deleted_at)
+        self.assertEqual(DeleteShareOwnerLogEntry.objects.count(), 0)
