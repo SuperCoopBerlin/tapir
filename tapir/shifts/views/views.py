84f69521dd3a6562e0b475b9295e77ebd95814bd
crosspolar <18083323+crosspolar@users.noreply.github.com>
2025-11-28 12:16:00 +0100

    Rule --> rule

--- tapir/shifts/views/views.py
+++ tapir/shifts/views/views.py
@@ -415,7 +415,7 @@ class CreateWatchRecurringShiftsView(
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
-        context_data["page_title"] = _("Create a Rule for recurring Shift-Watches")
+        context_data["page_title"] = _("Create a rule for recurring Shift-Watches")
         context_data["card_title"] = context_data["page_title"]
         return context_data
 
