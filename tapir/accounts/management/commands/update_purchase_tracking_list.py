e7fd7d543c4514ba6a71b3191d9ce52e450436e1
crosspolar <18083323+crosspolar@users.noreply.github.com>
2026-02-22 11:20:27 +0100

    remove tmp

--- tapir/accounts/management/commands/update_purchase_tracking_list.py
+++ tapir/accounts/management/commands/update_purchase_tracking_list.py
@@ -24,7 +24,6 @@ class Command(BaseCommand):
             mode="w",
             prefix="members-current-",
             suffix=".csv",
-            dir="/tmp",
         ) as temp_file:
             self.write_users_to_file(filename=temp_file.name)
             send_file_to_storage_server(temp_file.name, "u326634-sub4")
