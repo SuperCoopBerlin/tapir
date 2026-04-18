ea0635dd05defb9d47d308396af0d6a5b8686f22
crosspolar <18083323+crosspolar@users.noreply.github.com>
2026-04-18 09:42:26 +0200

    fix test

--- tapir/shifts/tests/test_shifttemplateenddateview.py
+++ tapir/shifts/tests/test_shifttemplateenddateview.py
@@ -3,6 +3,7 @@ import datetime
 from django.contrib.messages import get_messages
 from django.urls import reverse
 from django.utils import timezone
+from django_extensions.jobs import weekly
 
 from tapir.accounts.tests.factories.factories import TapirUserFactory
 from tapir.shifts.tests.factories import ShiftTemplateFactory
@@ -14,18 +15,22 @@ class TestShiftTemplateEndView(TapirFactoryTestBase):
 
     def setUp(self):
         super().setUp()
-        self.shift_template = ShiftTemplateFactory.create()
+        self.shift_template = ShiftTemplateFactory.create(
+            start_date=timezone.now().date(), weekday=0
+        )
         self.url = reverse(
             "shifts:shift_template_set_end_date", kwargs={"pk": self.shift_template.pk}
         )
 
-    def test_shiftTemplate_setEndDate_futureShiftsAfterEndDateAreCancelled(self):
+    def test_shiftTemplate_setEndDateInBetween_futureShiftsAfterEndDateAreCancelled(
+        self,
+    ):
         self.login_as_employee()
 
         user = TapirUserFactory.create(is_in_member_office=False)
         register_user_to_shift_template(self.client, user, self.shift_template)
 
-        today = timezone.now().today()
+        today = timezone.now().date()
         shift_1 = self.shift_template.create_shift_if_necessary(
             today + datetime.timedelta(days=7)
         )
@@ -59,7 +64,7 @@ class TestShiftTemplateEndView(TapirFactoryTestBase):
         shift_2.refresh_from_db()
         shift_3.refresh_from_db()
 
-        self.assertEqual(self.shift_template.end_date, end_date.date())
+        self.assertEqual(self.shift_template.end_date, end_date)
 
         self.assertFalse(shift_1.cancelled)
         self.assertFalse(shift_2.cancelled)
@@ -67,22 +72,28 @@ class TestShiftTemplateEndView(TapirFactoryTestBase):
         self.assertTrue(shift_3.cancelled)
         self.assertEqual(shift_3.cancelled_reason, cancellation_reason)
 
-    # def test_shiftTemplate_sendEndDateAfterAllShifts_noShiftIsCancelled(self):
-    #     self.login_as_employee()
-    #     today = timezone.now().today()
-    #     end_date = today + datetime.timedelta(days=365)
-    #     cancellation_reason = "Shift Template ended"
-    #
-    #     response = self.client.post(
-    #         self.url,
-    #         {
-    #             "end_date": end_date.strftime("%Y-%m-%d"),
-    #             "cancellation_reason": cancellation_reason,
-    #         },
-    #     )
-    #
-    #     self.shift_template.refresh_from_db()
-    #     self.assertEqual(self.shift_template.end_date, end_date.date())
-    #
-    #     msgs = list(get_messages(response.wsgi_request))
-    #     self.assertGreater(len(msgs), 0)
+    def test_shiftTemplate_sendEndDateAfterAllShifts_noShiftIsCancelled(self):
+        self.login_as_employee()
+        today = timezone.now().date()
+        end_date = today + datetime.timedelta(days=365)
+        cancellation_reason = "Shift Template ended"
+        shift = self.shift_template.create_shift_if_necessary(
+            today + datetime.timedelta(days=7)
+        )
+
+        response = self.client.post(
+            self.url,
+            {
+                "end_date": end_date.strftime("%Y-%m-%d"),
+                "cancellation_reason": cancellation_reason,
+            },
+        )
+
+        self.shift_template.refresh_from_db()
+        shift.refresh_from_db()
+        self.assertEqual(self.shift_template.end_date, end_date)
+
+        self.assertFalse(shift.cancelled)
+
+        msgs = list(get_messages(response.wsgi_request))
+        self.assertGreater(len(msgs), 0)
