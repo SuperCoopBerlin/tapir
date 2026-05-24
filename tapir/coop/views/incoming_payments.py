8ecb3e6cd1c29e1bbad0cb7c899a243448dcb8fe
Frederik <18083323+crosspolar@users.noreply.github.com>
2026-05-24 17:39:22 +0200

    feat: add csv-export to incoming payments for accounting (#786)
    * feat: add csv-export to incoming payments for accounting
    
    * translation

--- tapir/coop/views/incoming_payments.py
+++ tapir/coop/views/incoming_payments.py
@@ -10,6 +10,7 @@ from django.utils.translation import gettext_lazy as _
 from django.views import generic
 from django_filters.views import FilterView
 from django_tables2 import SingleTableView
+from django_tables2.export import ExportMixin
 
 from tapir.accounts.models import TapirUser
 from tapir.coop.forms import IncomingPaymentForm
@@ -80,11 +81,20 @@ class IncomingPaymentTable(django_tables2.Table):
     def render_paying_member(self, value, record: IncomingPayment):
         return self.render_member(self.request.user, record.paying_member)
 
+    def value_paying_member(self, value, record: IncomingPayment):
+        return record.paying_member
+
     def render_credited_member(self, value, record: IncomingPayment):
         return self.render_member(self.request.user, record.credited_member)
 
+    def value_credited_member(self, value, record: IncomingPayment):
+        return record.credited_member
+
     def render_created_by(self, value, record: IncomingPayment):
         return self.render_member(self.request.user, record.created_by.share_owner)
+
+    def value_created_by(self, value, record: IncomingPayment):
+        return record.created_by.share_owner
 
     def render_payment_date(self, value, record: IncomingPayment):
         return record.payment_date.strftime("%d.%m.%Y")
@@ -110,12 +120,14 @@ class IncomingPaymentFilter(django_filters.FilterSet):
     created_by = TapirUserModelChoiceFilter()
 
 
-class IncomingPaymentListView(LoginRequiredMixin, FilterView, SingleTableView):
+class IncomingPaymentListView(
+    LoginRequiredMixin, FilterView, ExportMixin, SingleTableView
+):
     table_class = IncomingPaymentTable
     model = IncomingPayment
     template_name = "coop/incoming_payment_list.html"
-
     filterset_class = IncomingPaymentFilter
+    export_formats = ["csv", "json"]
 
     def get_queryset(self):
         queryset = IncomingPayment.objects.all()
