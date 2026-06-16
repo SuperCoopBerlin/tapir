dbc18ee186364d6d44fd86adbdb1574d2beba175
crosspolar <18083323+crosspolar@users.noreply.github.com>
2026-06-16 21:48:44 +0200

    tests

--- tapir/shifts/tests/test_CreateWatchRecurringShiftsView.py
+++ tapir/shifts/tests/test_CreateWatchRecurringShiftsView.py
@@ -2,7 +2,11 @@ from django.urls import reverse
 
 from tapir.accounts.models import TapirUser
 from tapir.accounts.tests.factories.factories import TapirUserFactory
-from tapir.shifts.models import RecurringShiftWatch, StaffingStatusChoices
+from tapir.shifts.models import (
+    RecurringShiftWatch,
+    ShiftUserCapability,
+    StaffingStatusChoices,
+)
 from tapir.shifts.tests.factories import ShiftTemplateFactory
 from tapir.utils.tests_utils import TapirFactoryTestBase
 
@@ -131,3 +135,93 @@ class TestCreateWatchRecurringShiftsView(TapirFactoryTestBase):
 
         self.assertEqual(302, response.status_code)
         self.assertEqual(RecurringShiftWatch.objects.count(), 1)
+
+    def test_createRecurringShiftWatch_withWatchedCapabilities_entrySaved(self):
+        form_data = {
+            **self.default_form_data,
+            "weekdays": [1, 2],
+            "shift_template_group": ["A"],
+            "watched_capabilities": [
+                ShiftUserCapability.CASHIER,
+                ShiftUserCapability.BREAD_DELIVERY,
+            ],
+        }
+
+        response = self.client.post(
+            reverse(self.VIEW_NAME, args=[self.tapir_user.pk]), data=form_data
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(RecurringShiftWatch.objects.count(), 1)
+        created_watch = RecurringShiftWatch.objects.first()
+
+        self.assertEqual(
+            set(created_watch.watched_capabilities),
+            {ShiftUserCapability.CASHIER, ShiftUserCapability.BREAD_DELIVERY},
+        )
+
+    def test_createRecurringShiftWatch_noCapabilitiesSelected_entryCreated(self):
+        form_data = {
+            **self.default_form_data,
+            "weekdays": [1, 2],
+            "shift_template_group": ["A"],
+            "watched_capabilities": [],
+        }
+
+        response = self.client.post(
+            reverse(self.VIEW_NAME, args=[self.tapir_user.pk]), data=form_data
+        )
+
+        self.assertEqual(response.status_code, 302)
+        created_watch = RecurringShiftWatch.objects.first()
+        self.assertEqual(created_watch.watched_capabilities, [])
+
+    def test_createRecurringShiftWatch_invalidCapability_validationError(self):
+        form_data = {
+            **self.default_form_data,
+            "weekdays": [1, 2],
+            "shift_template_group": ["A"],
+            "watched_capabilities": ["invalid_capability"],
+        }
+
+        response = self.client.post(
+            reverse(self.VIEW_NAME, args=[self.tapir_user.pk]), data=form_data
+        )
+
+        form = response.context["form"]
+        self.assertFalse(form.is_valid())
+        self.assertIn("watched_capabilities", form.errors)
+
+    def test_createRecurringShiftWatch_withCapabilitiesAndStaffingStatus_entrySaved(
+        self,
+    ):
+        form_data = {
+            **self.default_form_data,
+            "weekdays": [1, 2],
+            "shift_template_group": ["A"],
+            "staffing_status": [
+                StaffingStatusChoices.UNDERSTAFFED,
+                StaffingStatusChoices.FULL,
+            ],
+            "watched_capabilities": [
+                ShiftUserCapability.CASHIER,
+                ShiftUserCapability.BREAD_DELIVERY,
+            ],
+        }
+
+        response = self.client.post(
+            reverse(self.VIEW_NAME, args=[self.tapir_user.pk]), data=form_data
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(RecurringShiftWatch.objects.count(), 1)
+        created_watch = RecurringShiftWatch.objects.first()
+
+        self.assertEqual(
+            set(created_watch.staffing_status),
+            {StaffingStatusChoices.UNDERSTAFFED, StaffingStatusChoices.FULL},
+        )
+        self.assertEqual(
+            set(created_watch.watched_capabilities),
+            {ShiftUserCapability.CASHIER, ShiftUserCapability.BREAD_DELIVERY},
+        )
